78f7f0d233c7884af257858812cf6089b95be7e7
GIB225 <101982666+GIB225@users.noreply.github.com>
2022-11-02 10:05:32 +0000

    Update and rename mainpage.html to index.php

--- Year2_CS_E_COMMERCE_WEBSITE/index.php
+++ Year2_CS_E_COMMERCE_WEBSITE/index.php
@@ -1,6 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
-
+<!--
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -44,7 +44,9 @@
         }
     </style>
 
-</head>
+</head> -->
+    
+   <?php include '/header.php'; ?>
 <!-- font-family: 'Staatliches', cursive; -->
 <!-- font-family: 'Russo One', sans-serif; -->
 
