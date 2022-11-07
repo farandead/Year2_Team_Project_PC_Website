a7bbdf720990813cb40609f0bb6924327cc40188
GIB225 <101982666+GIB225@users.noreply.github.com>
2022-11-07 02:48:35 +0000

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,21 @@
-<?php include "/Year2_Team_Project_PC_Website/phpScript/dbh.inc.php" ?>
+<?php
+//var_dump(YOU CAN SEE ME);
+$servername = "localhost";
+$username = "babimors_astonTeam";
+$password = ";^5z?6.mA9v"
+  //"wMxgHygDHh)z";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password);
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+} 
+echo "Connected successfully";
+
+
+
+?>
 <!DOCTYPE html>
 <html lang="en">
 
