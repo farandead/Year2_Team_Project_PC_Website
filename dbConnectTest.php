6efd655e5c1b18e42b7a4011fb414e89608953fa
GIB225 <101982666+GIB225@users.noreply.github.com>
2022-11-07 14:45:31 +0000

    Update dbConnectTest.php

--- dbConnectTest.php
+++ dbConnectTest.php
@@ -1,14 +1,3 @@
 <?php
-$servername = "localhost";
-$username = "babimors_astonTeam";
-$password = ";^5z?6.mA9v"
-
-try {
-  $conn = new PDO("mysql:host=$servername;dbname=babimors_compuk", $username, $password);
-  // set the PDO error mode to exception
-  $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-  echo "Connected successfully"; 
-} catch(PDOException $e) {
-  echo "Connection failed: " . $e->getMessage();
-}
+echo "TESTING TEST ";
 ?>
