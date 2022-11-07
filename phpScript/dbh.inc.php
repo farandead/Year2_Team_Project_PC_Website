4aa4a377518b5b2c103c4a1743d9257b0f9ac1a4
GIB225 <101982666+GIB225@users.noreply.github.com>
2022-11-07 02:35:14 +0000

    Update dbh.inc.php

--- phpScript/dbh.inc.php
+++ phpScript/dbh.inc.php
@@ -1,4 +1,5 @@
 <?php
+var_dump(YOU CAN SEE ME);
 $servername = "localhost";
 $username = "babimors_astonTeam";
 $password = "wMxgHygDHh)z";
@@ -11,4 +12,7 @@ if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 } 
 echo "Connected successfully";
+
+
+
 ?>
