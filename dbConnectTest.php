0a3a18450c22c445ad2c2fc4f0a91df3f39de50d
GIB225 <101982666+GIB225@users.noreply.github.com>
2022-11-07 14:58:56 +0000

    Update dbConnectTest.php

--- dbConnectTest.php
+++ dbConnectTest.php
@@ -1,3 +1,18 @@
 <?php
 echo "TESTING TEST ";
+
+
+$servername = "localhost";
+$username = "babimors_compuk";
+$password = ";^5z?6.mA9v";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password);
+
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+} 
+echo "Connected successfully";
+
 ?>
