710568f3a90701ac9476628f97fed1179a8a4448
UshakovKirill <116155318+UshakovKirill@users.noreply.github.com>
2022-11-08 09:51:17 +0000

    Create connect_db.php
    Connector for the Database - CompUK

---
+++ PHP/connect_db.php
@@ -0,0 +1,14 @@
+<?php
+$servername = "localhost";
+$username = 'babimors_astonTeam';
+$password ='HW#1o.5=#cl+';
+
+try {
+  $conn = new PDO("mysql:host=$servername;dbname=babimors_compuk", $username, $password);
+  // set the PDO error mode to exception
+  $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+  echo "Connected successfully"; 
+} catch(PDOException $e) {
+  echo "Connection failed: " . $e->getMessage();
+}
+?>
