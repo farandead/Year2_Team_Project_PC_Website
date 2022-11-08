25f7ec65e406a9ca062a63cac64a3f197051ba14
UshakovKirill <116155318+UshakovKirill@users.noreply.github.com>
2022-11-08 11:41:15 +0000

    Update connect_db.php

--- PHP/connect_db.php
+++ PHP/connect_db.php
@@ -3,11 +3,9 @@ $servername = "localhost";
 $username = 'babimors_astonTeam';
 $password ='HW#1o.5=#cl+';
 
-try {
-  $conn = new PDO("mysql:host=$servername;dbname=babimors_compuk", $username, $password);
-  // set the PDO error mode to exception
-  $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-} catch(PDOException $e) {
-  echo "Connection failed: " . $e->getMessage();
-}
+$conn=new mysqli($servername,$username,$password);
+
+if($conn->connect_error){
+  die("Connection failed: ".$conn->connect_error);
+echo "ok";
 ?>
