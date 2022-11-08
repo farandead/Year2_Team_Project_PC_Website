4bbc24279ba5d007d985a28373e2dd3d7c9e7c07
UshakovKirill <116155318+UshakovKirill@users.noreply.github.com>
2022-11-08 12:31:55 +0000

    Update TemplateProduct.php

--- PHP/TemplateProduct.php
+++ PHP/TemplateProduct.php
@@ -1,10 +1,10 @@
 <?
 $price=round($row['Product_Price']-$row['Product_Price']*$row['Product_Discount']/100,2);
-
+echo "0";
 echo "<div class=\"devices-sub-container-product\">
                         <div class=\"productdetails dark_target\">
                             <div class=\"product-details-title\">".$row['Product_Name']."</div>
-                            <div class=\"product-details-img\"><img src=\"Products/Images/".$row['Product_ID'].".JPG\" alt=\"No image\"></div>
+                            <div class=\"product-details-img\"><img src=\"../Products/Images/".$row['Product_ID'].".JPG\" alt=\"No image\"></div>
                             <div class=\"product-details-description dark_target\">
                                 <p>Price: <span>".$price."£</span> ";
                                 if($row['Product_Discount']!=0){
