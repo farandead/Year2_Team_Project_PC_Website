0baaedaabbbba0514c285df327c4cafccd3e6c81
UshakovKirill <116155318+UshakovKirill@users.noreply.github.com>
2022-11-08 12:20:24 +0000

    Update TemplateProduct.php

--- PHP/TemplateProduct.php
+++ PHP/TemplateProduct.php
@@ -4,11 +4,11 @@ $price=round($row['Product_Price']-$row['Product_Price']*$row['Product_Discount'
 echo "<div class=\"devices-sub-container-product\">
                         <div class=\"productdetails dark_target\">
                             <div class=\"product-details-title\">".$row['Product_Name']."</div>
-                            <div class=\"product-details-img\"><img src=\"img/Lafife Pro 15 6.webp\" alt=\"\"></div>
+                            <div class=\"product-details-img\"><img src=\"img/Lafife Pro 15 6.webp\" alt=\"No image\"></div>
                             <div class=\"product-details-description dark_target\">
                                 <p>Price: <span>".$price."£</span> ";
                                 if($row['Product_Discount']!=0){
-                               echo ",discount is ".$row['Product_Discount']."% (<s>".$row['Product_Price']."</s>)";
+                               echo ",discount is ".$row['Product_Discount']."% (<s>".$row['Product_Price']."£</s>)";
                                 }
                                 echo "
                                 </p>
