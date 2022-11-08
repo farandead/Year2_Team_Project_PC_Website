b03e9db099a4affbd2d5995bf7c2eb588f2a3e0b
GIB225 <101982666+GIB225@users.noreply.github.com>
2022-11-08 03:18:20 +0000

    Update sign_up.php

--- sign_up.php
+++ sign_up.php
@@ -8,9 +8,9 @@ try {
   $conn = new PDO("mysql:host=$servername;dbname=babimors_compuk", $username, $password);
   // set the PDO error mode to exception
   $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-  echo "Connected successfully"; 
+ // echo "Connected successfully"; 
 } catch(PDOException $e) {
-  echo "Connection failed: " . $e->getMessage();
+ // echo "Connection failed: " . $e->getMessage();
 }
 ?>
 <!-- END DATABASE CONNECTION-->
