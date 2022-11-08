59351f2f6a05875e9b1c9e9525f68b7c3e16b5b6
UshakovKirill <116155318+UshakovKirill@users.noreply.github.com>
2022-11-08 16:15:04 +0000

    Update PrintProducts.php

--- PHP/PrintProducts.php
+++ PHP/PrintProducts.php
@@ -4,7 +4,10 @@ require "connect_db.php";
 $queryCat=mysqli_query($conn,"SELECT Cat_ID FROM Category WHERE Cat_Name='$category'");
 
 $res=mysqli_fetch_assoc($queryCat);
-$Cat_ID=$res["Cat_ID"];
+if($res!=null){
+$Cat_ID=$res["Cat_ID"];}
+else{$Cat_ID=null;
+}
 
 $query=mysqli_query($conn,"SELECT * FROM Product WHERE Cat_ID='$Cat_ID'");
 
