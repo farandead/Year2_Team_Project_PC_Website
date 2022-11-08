b36454273b333451786a4aa17f84cf4de6a1856d
GIB225 <101982666+GIB225@users.noreply.github.com>
2022-11-08 21:42:46 +0000

    Update Sign_up_new.php

--- Sign_up_new.php
+++ Sign_up_new.php
@@ -36,6 +36,7 @@ try {
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Staatliches&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.3.0/font/bootstrap-icons.css" />
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     <link rel="stylesheet" href="css/sign_up_In.css">
     <script src="js/toggle_switch.js"></script>
 
