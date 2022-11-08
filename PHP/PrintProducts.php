9d74788a07929b7c9f768016af9d66c36a03b029
UshakovKirill <116155318+UshakovKirill@users.noreply.github.com>
2022-11-08 14:12:59 +0000

    Update PrintProducts.php

--- PHP/PrintProducts.php
+++ PHP/PrintProducts.php
@@ -4,7 +4,27 @@ require "connect_db.php";
 $sql = "SELECT Product_Name FROM Product";
 $query=mysqli_query($conn,"SELECT * FROM Product");
 
+$isEmpty=true;
+$counter=0;
+
 while($row=mysqli_fetch_assoc($query)){
+  
+  $isEmpty=false;
+  
+  if(counter==0){
+    echo "0";
+  }
+  
   require "TemplateProduct.php";
+  
+  counter=counter+1;
+  
+  if(counter==3){
+    echo "3";
+    counter=0;
+  }
+}
+if(isEmpty==true or counter!=0){
+  echo "yes"
 }
 ?>
