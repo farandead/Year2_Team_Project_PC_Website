a97e45071a98dad378dc5a9349e2e3ebcba1aa51
UshakovKirill <116155318+UshakovKirill@users.noreply.github.com>
2022-11-08 11:45:46 +0000

    Update connect_db.php

--- PHP/connect_db.php
+++ PHP/connect_db.php
@@ -2,8 +2,9 @@
 $servername = "localhost";
 $username = 'babimors_astonTeam';
 $password ='HW#1o.5=#cl+';
+$dbName="babimors_compuk";
 
-$conn=new mysqli($servername,$username,$password);
+$conn=new mysqli($servername,$username,$password,$dbName);
 
 if($conn->connect_error){
   die("Connection failed: ".$conn->connect_error);
