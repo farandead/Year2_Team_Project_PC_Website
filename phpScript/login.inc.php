392737297573ac1c2eaa0659920d633e03cf83c7
GIB225 <101982666+GIB225@users.noreply.github.com>
2022-11-08 02:45:45 +0000

    Update login.inc.php

--- phpScript/login.inc.php
+++ phpScript/login.inc.php
@@ -1,3 +1,18 @@
+<?php
+$servername = "localhost";
+$username = 'babimors_astonTeam';
+$password ='HW#1o.5=#cl+';
+
+try {
+  $conn = new PDO("mysql:host=$servername;dbname=babimors_compuk", $username, $password);
+  // set the PDO error mode to exception
+  $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+  echo "Connected successfully"; 
+} catch(PDOException $e) {
+  echo "Connection failed: " . $e->getMessage();
+}
+?>
+
 <?php 
 if (isset($_POST['submit_Login']) & !empty($_POST['submit_Login'])) {
 // PHP Form Validations
