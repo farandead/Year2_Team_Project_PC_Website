f84f8dc0b22f0466592ea16f71053f5a7ed499a8
UshakovKirill <116155318+UshakovKirill@users.noreply.github.com>
2022-11-08 11:13:47 +0000

    Update PrintProducts.php

--- PHP/PrintProducts.php
+++ PHP/PrintProducts.php
@@ -1,9 +1,9 @@
 <?
 require "connect_db.php"
   
-  
-$query= 1;
+$sql="SELECT Car_Type_ID FROM car_type_table ";
 
+$query=mysqli_query($conn,$sql);
 
 while($row=mysqli_fetch_assoc($query)){
   echo".$row['Product_Name']"
