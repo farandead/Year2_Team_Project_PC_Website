fa56a21d978027f787da605d383c506752fc6267
UshakovKirill <116155318+UshakovKirill@users.noreply.github.com>
2022-11-08 11:28:48 +0000

    Update PrintProducts.php

--- PHP/PrintProducts.php
+++ PHP/PrintProducts.php
@@ -9,7 +9,6 @@ $sql = "SELECT Product_Name FROM Product";
 $query=mysqli_query($conn,$sql);
 
 while($row=mysqli_fetch_assoc($query)){
-  
-  echo".$row['Product_Name'].";
+  echo "test".$row['Product_Name']."test";
 }
 ?>
