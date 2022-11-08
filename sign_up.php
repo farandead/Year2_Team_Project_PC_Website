3f93e775c633acd23d7d865b4ed5175e2b59e240
GIB225 <101982666+GIB225@users.noreply.github.com>
2022-11-08 01:52:36 +0000

    Update sign_up.php

--- sign_up.php
+++ sign_up.php
@@ -1,3 +1,23 @@
+<!-- DATABASE CONNECTION -- HAVE TO MAKE AN INCLUSIONS-->
+<?php
+echo "TESTING TEST Kir";
+
+
+$servername = "localhost";
+$username = 'babimors_astonTeam';
+$password ='HW#1o.5=#cl+';
+
+try {
+  $conn = new PDO("mysql:host=$servername;dbname=babimors_compuk", $username, $password);
+  // set the PDO error mode to exception
+  $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+  echo "Connected successfully"; 
+} catch(PDOException $e) {
+  echo "Connection failed: " . $e->getMessage();
+}
+?>
+<!-- END DATABASE CONNECTION-->
+
 <html>
 
 <head>
@@ -87,18 +107,118 @@
         </div>
 
     </header>
+<!-- SIGN UP PHP SCRIPT-->
+    <?php 
 
+if (isset($_POST['signup_submit']) & !empty($_POST['signup_submit'])) {
+	
+    //if (empty($_POST['lastName'])) {
+       // $errors[] = "";
+    //}
+    if (empty($_POST['firstName'])) {
+        $errors[] = "Please Enter a Name";
+    }
+    if (empty($_POST['email'])) {
+        $errors[] = "E-mail address is required";
+    } else {
+	   
+        // Check Email is Unique with DB Query
+	    
+        $sql = "SELECT * FROM account WHERE user_Email=?";
+     	  $result = $conn->prepare($sql);
+        $result->execute(array($_POST['email']));
+        $count = $result->rowCount();
+        if ($count == 1) {
+            $errors[] = "The e-mail already exist. Please connect.";
+        }
+    }
+
+   // if (empty($_POST['phoneNumber'])) {
+      //  $errors[] = "Veuiller rentrer votre numéro de téléphone";
+   // }
+    if (empty($_POST['pwd'])) {
+        $errors[] = "Please insert a password.";
+    } else {
+      $pass_hash = password_hash($_POST['pwd'], PASSWORD_DEFAULT);
+        // check the repeat password
+       // if (empty($_POST['pwdRepeat'])) {
+         //  $errors[] = "Confirmer votre mot de passe.";
+       // } else {
+            // compare both passwords, if they match. Generate the Password Hash
+          //  if ($_POST['pwd'] == $_POST['pwdRepeat']) {
+                // create password hash
+         //       $pass_hash = password_hash($_POST['pwd'], PASSWORD_DEFAULT);
+          //  } else {
+                // Display Error Message
+             //   $errors[] = "Les mots de passes doievent être identiques";
+            //}
+	    var_dump($_POST['firstName']);
+		var_dump($_POST['email']);
+	    var_dump($pass_hash);
+       // }
+    }
+
+    if (empty($errors)) {
+		echo " inserting ";
+			
+      //  $longueurKey = 15;
+      //  $key = "";
+       // for ($i = 1; $i < $longueurKey; $i++) {
+       //     $key .= mt_rand(0, 9);
+        //}
+
+        $sql = "INSERT INTO account (User_FName, user_Email, User_Password) VALUES ( :User_FName, :user_Email, :User_Password)";
+        $result = $conn->prepare($sql);
+$values = array(
+           
+            ':User_FName'     => $_POST['firstName'],
+            ':user_Email'        => $_POST['email'],
+           ':User_Password'     => $pass_hash,
+           
+        );
+        $res = $result->execute($values);
+        if ($res) {
+          echo "YOU ARE IN !";
+      //      $messages[] = "YOU ARE IN !";
+     //       $messages[]= 'An confirmation e-mail have been send to this address' .$_POST['email']. '. ';
+           /*
+            if ($confirmationMail ){
+                $messages[]= 'Un e-mail de confirmation à été envoyé à l&apos;addresse suivante : ' .$_POST['email']. '. ';
+            } else{
+                $errors[] = " EMAIL NOT SEND ";
+            }
+            */
+        } else {
+	echo"not in ";
+	
+	}
+    }else {
+	    
+	    echo "not inserting";
+	    var_dump($errors);
+    }
+
+     //var_dump($errors);
+    // echo "</br>";
+
+    // var_dump($messages);
+}
+?>
+<!-- END --- SIGN UP PHP SCRIPT-->
 
     <div class="container" id="container123">
         <div class="form-container sign-up-container">
-            <form action="#" class="form-id">
+            <form action="#" class="form-id" method="post">
                 <h1>Create Account</h1>
-                <input type="text" placeholder="Name" />
-                <input type="email" placeholder="Email" />
-                <input type="password" placeholder="Password" />
-                <button>Sign Up</button>
+                <input type="text" name="firstName" placeholder="Name" value=""  />
+                <input type="email" name="email" placeholder="Email" value=""  />
+                <input type="password" name="pwd" placeholder="Password" value=""  />
+               <input class="btn btn-block" type="submit" name="signup_submit" value="Sign Up">
             </form>
         </div>
+       
+        
+        
         <div class="form-container sign-in-container">
             <form action="#" style="background:white ;">
                 <h1>Sign in</h1>
