236d51f49241dafc734c9ffc7f36ea0903affe1e
UshakovKirill <116155318+UshakovKirill@users.noreply.github.com>
2022-11-09 10:13:47 +0000

    Update PrintProducts.php

--- PHP/PrintProducts.php
+++ PHP/PrintProducts.php
@@ -1,17 +1,6 @@
 <?php
 require "connect_db.php";
-
-$queryCat=mysqli_query($conn,"SELECT Cat_ID FROM Category WHERE Cat_Name='$category'");
-$res=mysqli_fetch_assoc($queryCat);
-
-if($res!=null){
-$Cat_ID=$res["Cat_ID"];}
-else{$Cat_ID=null;
-}
-
-$query=mysqli_query($conn,"SELECT * FROM Product WHERE Cat_ID='$Cat_ID'");
-
-$template= "TemplateProduct.php";
+require "ProductsSQL.php";
 
 
 
