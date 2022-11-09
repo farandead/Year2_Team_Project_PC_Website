6a9e1db3caa6634977fee404a9fda30b5ef519d2
GIB225 <101982666+GIB225@users.noreply.github.com>
2022-11-09 08:09:51 +0000

    Update Sign_up_new.php

--- Sign_up_new.php
+++ Sign_up_new.php
@@ -337,8 +337,8 @@ try {
                 if ($count == 1) {
                     // Compare the password with password hash
                     
-                    $hashed = '$2y$10$ATrMted2kYz/iT/YWUp1euF';
-                    if (password_verify($_POST['pwd'], $hashed)) {
+                    //$hashed = '$2y$10$ATrMted2kYz/iT/YWUp1euF';
+                    if (password_verify($_POST['pwd'], $res['User_Password'])) {
                         // regenerate session id
                         //session_start();
                         // $_SESSION['login'] = true;
