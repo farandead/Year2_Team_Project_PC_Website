1237c8f1751534e57ac36b1d19383e8052b4bdab
UshakovKirill <116155318+UshakovKirill@users.noreply.github.com>
2022-11-10 10:09:53 +0000

    Update Categories_Page.php

--- Categories_Page.php
+++ Categories_Page.php
@@ -1,2 +1,2 @@
 <-- duplicate of the product_Page.php -->
-<a href='product_page.php?Category=Acc'><button>Click</button></a>
+<a href='product_page.php?Category=Accessories'><button>Click</button></a>
