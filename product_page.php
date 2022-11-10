befed5a3d5a5e794218c0d66f91bd1c965b40a2b
UshakovKirill <116155318+UshakovKirill@users.noreply.github.com>
2022-11-10 11:01:27 +0000

    Update product_page.php

--- product_page.php
+++ product_page.php
@@ -102,7 +102,7 @@
             
             $category=$_GET["Category"];
             if($category==null){
-            Header('Location: index.php');
+            header('Location:index.php');
                 exit;
             }
                 echo "<h1>".$category."</h1>";
