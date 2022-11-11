9728c74480b9e6c7e8182b0f68688144579009b8
UshakovKirill <116155318+UshakovKirill@users.noreply.github.com>
2022-11-11 11:43:32 +0000

    Update textCutter.php

--- PHP/textCutter.php
+++ PHP/textCutter.php
@@ -2,6 +2,16 @@
                     if($Changes>0){
                     for ($x = 1; $x <= $Changes; $x++) {
                      $position=$Jump*$x;
+                      $tracker=false;
+                      while($tracker){
+                      if($text[$position+1]!=" "){
+                      $position=$position+1;
+                      }
+                      else{
+                      $tracker=true;
+                      }
+                      }
+                      }
                        $text= substr_replace(  $text,  $insert, $position, 0 );
                         $Jump=$Jump+Strlen($insert);
                                                    }
