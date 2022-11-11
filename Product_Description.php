6bfa94c373408eb6dfafc0e706b9de606281763e
UshakovKirill <116155318+UshakovKirill@users.noreply.github.com>
2022-11-11 10:52:14 +0000

    Update Product_Description.php

--- Product_Description.php
+++ Product_Description.php
@@ -110,7 +110,19 @@
                 </div>
                
                 <?php
-                require "PHP/IndProductPriceDis.php";
+                $price=round($res['Product_Price']-$res['Product_Price']*$res['Product_Discount']/100,2);
+
+
+echo "
+<div class=\"summary-container price-container dark_target\">
+                    <p><strong>£".$price." </strong></p>
+                </div>";
+if($res['Product_Discount']>0){
+echo "
+                <div class=\"summary-container price-container dark_target\">
+                    <p><strong>discount is ".$res['Product_Discount']."%  </strong>(<s>".$res['Product_Price']."£</s>)</p>
+                </div>";
+}
                 ?>
                 
                 
