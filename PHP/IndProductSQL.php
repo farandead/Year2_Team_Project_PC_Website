33384c2d11ca5a9619c83e3a372c96edfe36d72f
UshakovKirill <116155318+UshakovKirill@users.noreply.github.com>
2022-11-11 09:05:27 +0000

    Update IndProductSQL.php

--- PHP/IndProductSQL.php
+++ PHP/IndProductSQL.php
@@ -1,8 +1,9 @@
 <?php
 //SQL TO ACCESS THE INPUT PRODUCT AND COLLECT THE DATA INTO AN ARRAY
 
-  
-  $queryPr=mysqli_query($conn,"SELECT * FROM Product WHERE Product_ID='$category'");
+$product=$_GET["Product"];  
+
+  $queryPr=mysqli_query($conn,"SELECT * FROM Product WHERE Product_ID='$product'");
   $res=mysqli_fetch_assoc($queryPr);
 
 if($res!=null){
