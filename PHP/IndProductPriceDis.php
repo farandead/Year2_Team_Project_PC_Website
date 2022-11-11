baac3f0685e2526ed10d39f4470b3ae9e4cf381e
UshakovKirill <116155318+UshakovKirill@users.noreply.github.com>
2022-11-11 10:40:27 +0000

    Update IndProductPriceDis.php

--- PHP/IndProductPriceDis.php
+++ PHP/IndProductPriceDis.php
@@ -1,4 +1,16 @@
 <?php
-<!--Print price and disscount
-  -->
+$price=round($res['Product_Price']-$res['Product_Price']*$res['Product_Discount']/100,2);
+
+
+echo "
+<div class=\"summary-container price-container dark_target\">
+                    <p><strong>£".$price." </strong></p>
+                </div>";
+if($res['Product_Discount']>0){
+echo "
+                <div class=\"summary-container price-container dark_target\">
+                    <p><strong>£600 </strong>inl. VAT</p>
+                </div>";
+}
+
 ?>
