e1cdb9f49369200bb77909537a06b009bfd87ea0
UshakovKirill <116155318+UshakovKirill@users.noreply.github.com>
2022-11-11 10:12:58 +0000

    Update IndProductSQL.php

--- PHP/IndProductSQL.php
+++ PHP/IndProductSQL.php
@@ -7,7 +7,11 @@ $product=$_GET["Product"];
   $res=mysqli_fetch_assoc($queryPr);
 
 if($res!=null){
-$Product_ID=$res["Product_ID"];}
+$Product_ID=$res["Product_ID"];
+  $Man_ID=$res["Man_ID"];
+  $queryPr2=mysqli_query($conn,"SELECT * FROM Manufacture WHERE Man_ID='$Man_ID'");
+  $res2=mysqli_fetch_assoc($queryPr2);
+}
 else{$Product_ID=null;
 }
 ?>
