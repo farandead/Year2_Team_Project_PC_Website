8d40374f1a80f25123c012994ee526d58d678ff8
UshakovKirill <116155318+UshakovKirill@users.noreply.github.com>
2022-11-11 12:42:48 +0000

    Update TemplateCategories.php

--- PHP/TemplateCategories.php
+++ PHP/TemplateCategories.php
@@ -1,5 +1,21 @@
 <?php
 
-echo $row['Cat_Name'];
-
+//echo $row['Cat_Name'];
+echo"
+                <div class =\"category-details\">
+                    <div class=\"category-details-title\">Desktops</div>
+                    <div class=\"category-details-img1\"><img src=\"img/AGILITY_PRO.webp\" alt=\"No image\"></div>
+                    <div class=\"category-details-description\">
+                        <ul class=\"description-ulisted\">
+                            <li class=\"description-ulisted-item\">High End Desktops</li>
+                            <li class=\"description-ulisted-item\">Wide variety of specs</li>
+                            <li class=\"description-ulisted-item\">Bargain Prices</li>
+                        </ul>
+                    </div>
+                    <div class=\"category-details-browse-button\">
+                        <button>BROWSE DESKTOPS ></button>
+                    </div>
+                </div>
+            </div>
+";
 ?>
