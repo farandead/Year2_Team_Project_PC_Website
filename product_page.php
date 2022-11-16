a734a2c502669d89d521f30d4146009f0e0a6cfc
UshakovKirill <116155318+UshakovKirill@users.noreply.github.com>
2022-11-16 02:27:46 +0000

    Update product_page.php

--- product_page.php
+++ product_page.php
@@ -109,7 +109,7 @@ require "PHP/StatusSetter.php";
                 exit();
             }
                 echo "<h1>".$category."</h1>";
-                require "Adminview.php";
+                require "PHP/Adminview.php";
             
             ?>
                 
