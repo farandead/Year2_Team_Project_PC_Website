24cad626925d8a0491a3c8981db47da2854d173a
UshakovKirill <116155318+UshakovKirill@users.noreply.github.com>
2022-11-16 02:37:08 +0000

    Create AdminviewCt.php

---
+++ PHP/AdminviewCt.php
@@ -0,0 +1,22 @@
+<?php
+$linkToAdd="";
+
+
+if($status=="Admin")
+ 
+
+if($view!=1){
+//change to 1
+  $link="product_page.php?Category=".$CATT."&AdminView=1";
+}
+else{
+//change to 0
+   $link="product_page.php?Category=".$CATT."";
+}
+
+if($view==1){
+  // echo add buton
+  echo "<a href='".$linkToAdd."'>add </a>";
+}
+echo "<a href='".$link."'>change view</a>";
+?>
