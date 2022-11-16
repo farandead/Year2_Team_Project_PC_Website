8051d45822f99c4f7ad93017edbdfa1f97177c45
UshakovKirill <116155318+UshakovKirill@users.noreply.github.com>
2022-11-16 02:24:34 +0000

    Update Adminview.php

--- PHP/Adminview.php
+++ PHP/Adminview.php
@@ -1,5 +1,18 @@
 <?php
+if($status=="Admin")
+if($view!=1){
+//change to 1
+ $CATT= $_GET["Category"];
+  $link="product_page.php?Category=".$CATT."&AdminView=1";
+}
+else{
+//change to 0
+   $link="product_page.php?Category=".$CATT."";
+}
 
-
-
+if($view==1){
+  // echo add buton
+  echo "<a href=''>add </a>";
+}
+echo "<a href='".$link."'>change view</a>";
 ?>
