27a5c7e72a644a8f79edee2ab281139fe530e041
UshakovKirill <116155318+UshakovKirill@users.noreply.github.com>
2022-11-21 12:14:42 +0000

    Update basketSignedOut.php

--- PHP/basketSignedOut.php
+++ PHP/basketSignedOut.php
@@ -1,6 +1,7 @@
 <?php
 $_COOKIE["Basket"]="8,1;2,3;";
 $product="8";
+$IsThere=false;
 
 if(isset($_COOKIE["Basket"])){
 $BasketStr= $_COOKIE["Basket"];
@@ -13,16 +14,26 @@ $ArrayCollectionBasket[$i]=explode(",", $arrayStringBasket[$i] );
   
   
   if($ArrayCollectionBasket[$i][0]==$product){
+    
+    echo "Found: ";
  echo $ArrayCollectionBasket[$i][0];
+    $IsThere=true;
   break;
  }
   
   
 }
-
-echo "Done1";
+echo "Not found!";
 }else{
 echo "empty";
+  $_COOKIE["Basket"]="";
 }
+
+if(!$IsThere){
+  $_COOKIE["Basket"]=$_COOKIE["Basket"]+$product.",1;"
+}
+
+echo "Basket: ";
+echo $_COOKIE["Basket"];
 
 ?>
