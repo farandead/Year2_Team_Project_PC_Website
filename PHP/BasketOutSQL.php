0a2e5093d8ba33deac012a7baede92d183ea340a
UshakovKirill <116155318+UshakovKirill@users.noreply.github.com>
2022-11-21 22:41:19 +0000

    Update BasketOutSQL.php

--- PHP/BasketOutSQL.php
+++ PHP/BasketOutSQL.php
@@ -10,13 +10,17 @@ $arrayStringBasket=  explode(";", $BasketStr );
 for($i=0;$i<Count($arrayStringBasket);$i++){
 $ArrayCollectionBasket=explode(",", $arrayStringBasket[$i] );
   $ArrayCollectionBasketDic=array($ArrayCollectionBasket[0]=>$ArrayCollectionBasket[1]);
-  $items=$items.$ArrayCollectionBasket[0];
+  
+  if($i==0){ $items=$items."Product_ID=\'".$ArrayCollectionBasket[0]."\'";}else{
+   $items=$items." OR Product_ID=\'".$ArrayCollectionBasket[0]."\'";
+  }
+  
   
 }
 
 echo $items;
 
- $queryPr=mysqli_query($conn,"SELECT * FROM Product WHERE Product_ID='$product'");
+ $queryPr=mysqli_query($conn,"SELECT * FROM Product WHERE ".$items."");
   $res=mysqli_fetch_assoc($queryPr);
 
 ?>
