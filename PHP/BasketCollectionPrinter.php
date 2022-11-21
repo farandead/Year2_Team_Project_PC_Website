4ce0092721f48e7bc43939eff50fb09a04736751
UshakovKirill <116155318+UshakovKirill@users.noreply.github.com>
2022-11-21 22:31:25 +0000

    Update BasketCollectionPrinter.php

--- PHP/BasketCollectionPrinter.php
+++ PHP/BasketCollectionPrinter.php
@@ -5,6 +5,11 @@ if(isset($_SESSION['User_Status'])){
 if(isset($_COOKIE["BasketCompUK"])){
 if($_COOKIE["BasketCompUK"]!=""){
 echo "Products";
+  
+require "connect_db.php";
+require "BasketOutSQL.php";
+require "Printer.php";
+  
 }else{
   echo "No products";}
 }
