06b4fbee123beed0f2cb925d05cc879bbc662df4
UshakovKirill <116155318+UshakovKirill@users.noreply.github.com>
2022-11-21 22:30:48 +0000

    Create BasketOutSQL.php

---
+++ PHP/BasketOutSQL.php
@@ -0,0 +1,22 @@
+<?php
+
+$BasketStr= $_COOKIE["BasketCompUK"];
+
+$items="";
+
+$arrayStringBasket=  explode(";", $BasketStr );
+
+
+for($i=0;$i<Count($arrayStringBasket);$i++){
+$ArrayCollectionBasket=explode(",", $arrayStringBasket[$i] );
+  $ArrayCollectionBasketDic=array($ArrayCollectionBasket[0]=>$ArrayCollectionBasket[1]);
+  $items=$items+$ArrayCollectionBasket[0];
+  
+}
+
+echo $items;
+
+ $queryPr=mysqli_query($conn,"SELECT * FROM Product WHERE Product_ID='$product'");
+  $res=mysqli_fetch_assoc($queryPr);
+
+?>
