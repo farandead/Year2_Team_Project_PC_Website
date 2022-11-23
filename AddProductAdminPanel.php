b54d517d8795ff25c1d551feda5c294984b519a7
UshakovKirill <116155318+UshakovKirill@users.noreply.github.com>
2022-11-23 21:18:08 +0000

    Update AddProductAdminPanel.php

--- AddProductAdminPanel.php
+++ AddProductAdminPanel.php
@@ -55,7 +55,7 @@ $queryCats=mysqli_query($conn,"SELECT * FROM Category");
         
         
         
-        <textarea class="scrollabletextbox" name="note" required>
+        <textarea class="scrollabletextbox" name="note"  cols="129" maxlength="5000" required>
   
 </textarea>
         <br>
