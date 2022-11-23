9d3b8d2726a0df18067cbc1078b125a665e8f34d
UshakovKirill <116155318+UshakovKirill@users.noreply.github.com>
2022-11-23 15:58:12 +0000

    Update RemoveBasketOut.php

--- PHP/RemoveBasketOut.php
+++ PHP/RemoveBasketOut.php
@@ -6,7 +6,7 @@ if(isset($_COOKIE["BasketCompUK"])){
 $bas=$_COOKIE["BasketCompUK"];
 echo $bas;
   
-  $bas=preg_replace("/10,[1-9]+;/","",$bas):
+  $bas=preg_replace("/10,[1-9]+;/","",$bas);
   echo " After:";
   echo $bas;
 }
