d047fcb57618b468bd842f31b7da51b9d2c1df31
UshakovKirill <116155318+UshakovKirill@users.noreply.github.com>
2022-11-23 18:58:02 +0000

    Update AddProductAdminPanel.php

--- AddProductAdminPanel.php
+++ AddProductAdminPanel.php
@@ -8,11 +8,26 @@ require "PHP/CheckAdminAccess.php";
     <form action="">
   <label for="Name">Name:</label><br>
   <input type="text" id="NamePr" name="NamePr" maxlength= "50" ><br>
+        
   <label for="Price">Price:</label><br>
   <input type="number" id="PricePr" name="PricePr" min="0.01" max = "1000000" ><br><br>
+        
   <label for="Discount">Discount:</label><br>
   <input type="number" id="DisPr" name="DisPr" min="0" max="100" ><br><br>
+  
+     <label for="Cats">Choose a Category:</label>
+  <select name="Cats" id="cars">
+    <option value="volvo">Volvo</option>
+    <option value="saab">Saab</option>
+  </select>
         
+   <label for="Mans">Choose a Manufacture:</label>
+  <select name="Mans" id="Mans">
+    <option value="volvo">Volvo</option>
+    <option value="saab">Saab</option>
+  </select>
+        
+        <a> <button type="buttonMan">Add a new manufacture</button> </a>
         
   <input type="submit" value="Submit">
 </form> 
