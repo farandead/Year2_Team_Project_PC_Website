81b8a88f0a7987e8a40829d38487c804a93e5aea
UshakovKirill <116155318+UshakovKirill@users.noreply.github.com>
2022-11-23 18:45:27 +0000

    Update AddProductAdminPanel.php

--- AddProductAdminPanel.php
+++ AddProductAdminPanel.php
@@ -6,10 +6,14 @@ require "PHP/CheckAdminAccess.php";
 
 <body>
     <form action="">
-  <label for="fname">First name:</label><br>
-  <input type="text" id="fname" name="fname" ><br>
-  <label for="lname">Last name:</label><br>
-  <input type="text" id="lname" name="lname" ><br><br>
+  <label for="Name : ">First name:</label><br>
+  <input type="text" id="NamePr" name="NamePr" ><br>
+  <label for="Price : ">Last name:</label><br>
+  <input type="number" id="PricePr" name="PricePr" min="0.01" ><br><br>
+  <label for="Discount : ">Last name:</label><br>
+  <input type="number" id="DisPr" name="DisPr" min="0" max="100" ><br><br>
+        
+        
   <input type="submit" value="Submit">
 </form> 
 </body>
