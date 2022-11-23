b71260597c04edb2d83c71fc554d15d5e37062e0
UshakovKirill <116155318+UshakovKirill@users.noreply.github.com>
2022-11-23 17:27:38 +0000

    Update ProductsIndexSQL.php

--- PHP/ProductsIndexSQL.php
+++ PHP/ProductsIndexSQL.php
@@ -1,6 +1,6 @@
 <?php
 
-$query=mysqli_query($conn,"SELECT * FROM Product");
+$query=mysqli_query($conn,"SELECT * FROM Product WHERE Cat_ID = '1' OR Cat_ID = '3'");
 
 $template= "TemplateProductIndex.php";
 
