f60db3c9b033ae2a4608151863ec19870423a097
UshakovKirill <116155318+UshakovKirill@users.noreply.github.com>
2022-11-23 17:52:00 +0000

    Update index.php

--- index.php
+++ index.php
@@ -170,7 +170,7 @@
             
             <?php
     //here
-    require "connect_db.php";
+    require "PHP/connect_db.php";
     $query=mysqli_query($conn,"SELECT * FROM Product WHERE (Cat_ID = '1' OR Cat_ID = '3') AND Product_Discount > 0");
     require "PHP/ProductsCollectionPrinterIndex.php";
     ?>
