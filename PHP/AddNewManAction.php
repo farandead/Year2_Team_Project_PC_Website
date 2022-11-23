c25bb6dddbded4cf7a091563a1c3989f845251b8
UshakovKirill <116155318+UshakovKirill@users.noreply.github.com>
2022-11-23 20:34:51 +0000

    Update AddNewManAction.php

--- PHP/AddNewManAction.php
+++ PHP/AddNewManAction.php
@@ -4,7 +4,20 @@ if (isset($_POST['submit'])){
 $link=$_POST['LinkMan'];
 $name=$_POST['NameMan'];
   
-  echo $name.$link;
+ require "PHP/connect_db.php";
+  
+  //SQL to add a new Man
+  
+  
+  $sql = "INSERT INTO Manufacture (Man_Name, Man_Link)
+VALUES ('".$name."', '".$link."')";
+
+if ($conn->query($sql) === TRUE) {
+  echo "New record created successfully";
+} else {
+  echo "Error: " . $sql . "<br>" . $conn->error;
+}
+
   
 }
 
