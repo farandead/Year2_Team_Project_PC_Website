71149050c7711411be6d399e213ce3f8c1dc18c0
UshakovKirill <116155318+UshakovKirill@users.noreply.github.com>
2022-11-23 17:53:47 +0000

    Update index.php

--- index.php
+++ index.php
@@ -179,6 +179,14 @@
         </div>
 
         <div class="devices-container-laptop" id="laptop_container">
+            
+                        <?php
+    //here
+    require "PHP/connect_db.php";
+    $query=mysqli_query($conn,"SELECT * FROM Product WHERE (Cat_ID = '1' OR Cat_ID = '3') AND Product_Discount > 0");
+    require "PHP/ProductsCollectionPrinterIndex.php";
+    ?>
+            
             <div class="devices-sub-container">
                 <div class="devices-sub-container-product">
                     <div class="productdetails">
