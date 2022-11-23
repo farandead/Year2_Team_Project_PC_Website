7703bf5f1600bf8840199ac3ac320754e137ed22
UshakovKirill <116155318+UshakovKirill@users.noreply.github.com>
2022-11-23 18:52:01 +0000

    Update AddProductAdminPanel.php

--- AddProductAdminPanel.php
+++ AddProductAdminPanel.php
@@ -9,7 +9,7 @@ require "PHP/CheckAdminAccess.php";
   <label for="Name">Name:</label><br>
   <input type="text" id="NamePr" name="NamePr" maxlength= "50" ><br>
   <label for="Price">Price:</label><br>
-  <input type="number" id="PricePr" name="PricePr" min="0.01" ><br><br>
+  <input type="number" id="PricePr" name="PricePr" min="0.01" max = "1000000" ><br><br>
   <label for="Discount">Discount:</label><br>
   <input type="number" id="DisPr" name="DisPr" min="0" max="100" ><br><br>
         
