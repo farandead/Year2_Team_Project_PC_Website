280a20031f7ae02423c8fda2b9d0908d8ebaefd4
UshakovKirill <116155318+UshakovKirill@users.noreply.github.com>
2022-11-24 16:08:14 +0000

    Update show.php

--- PHP/TEST/show.php
+++ PHP/TEST/show.php
@@ -4,7 +4,7 @@ require "../connect_db.php";
 $sql = "SELECT * FROM TESTUPLOAD WHERE NAME='kj'";
 $sth = $conn->query($sql);
 $result=mysqli_fetch_array($sth);
-echo $result['img'];
+//echo $result['img'];
 echo '<img src="data:image/jpg;base64,'.base64_encode( $result['img'] ).'"/>';
 
 ?>
