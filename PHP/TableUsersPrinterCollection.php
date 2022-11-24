09a0bd88ac62f52de5f29a431871e877ef746b98
UshakovKirill <116155318+UshakovKirill@users.noreply.github.com>
2022-11-24 21:43:39 +0000

    Update TableUsersPrinterCollection.php

--- PHP/TableUsersPrinterCollection.php
+++ PHP/TableUsersPrinterCollection.php
@@ -3,8 +3,18 @@
 require "connect_db.php";
 
 $templateRow="";
-$sql="";
+$sql="SELECT User_ID, user_Email, User_SName, User_FName,User_Phone_Number FROM account";
+  $query=mysqli_query($conn, $sql);
+while($row=mysqli_fetch_assoc($query)){
+echo 
+                    "<tr>
+                      <td>".$row['User_ID']."</td>
+                      <td>".$row['user_Email']."</td>
+                      <td>".$row['User_Phone_Number']."</td>
+                      <td>".$row['User_SName']." "..$row['User_FName'].."</td>
+                  </tr>";
 
+}
 
 $conn  -> close();
 
