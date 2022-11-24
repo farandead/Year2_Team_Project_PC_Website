14639527c9650be9e503cd337af39bdab68dcaeb
UshakovKirill <116155318+UshakovKirill@users.noreply.github.com>
2022-11-24 16:43:15 +0000

    Update TemplateProduct.php

--- PHP/TemplateProduct.php
+++ PHP/TemplateProduct.php
@@ -4,7 +4,9 @@ $price=round($row['Product_Price']-$row['Product_Price']*$row['Product_Discount'
 echo "<div class=\"devices-sub-container-product\">
                         <div class=\"productdetails dark_target\">
                             <div class=\"product-details-title\">".$row['Product_Name']."</div>
-                            <div class=\"product-details-img\"><img src=\"data:image/jpg;base64,'.base64_encode( $row['img'] ).'\"/  width=\"150 px\" height=\"150px\" alt=\"No image\">
+                            <div class=\"product-details-img\">";
+echo '<img src="data:image/png;base64,'.base64_encode( $row['img'] ).'"/  width="150 px" height="150px" alt="No image">';
+                            echo "
                             </div>
                             <div class=\"product-details-description dark_target\">
                                 <p>Price: <span>".$price."£</span> ";
