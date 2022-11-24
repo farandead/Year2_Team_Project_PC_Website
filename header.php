680b89da65e4f2ed133c01ff0ab864b645e41181
GIB225 <101982666+GIB225@users.noreply.github.com>
2022-11-24 13:25:33 +0000

    Update header.php

--- header.php
+++ header.php
@@ -1,4 +1,4 @@
-<head>
+<!-- <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -44,7 +44,7 @@
 </head>
 
 <body style="background: none;" class="dark_target"></body>
-   
+   -->
 
     <header class="navigationBar" id="navigationLightMode"  >
         <ul id="navbar">
@@ -89,4 +89,4 @@
 
     </header>
 
-</body>
+
