541a731bc8e68a028b835d3629cb1c6142f1a490
UshakovKirill <116155318+UshakovKirill@users.noreply.github.com>
2022-11-24 18:35:24 +0000

    Update AddNewCatAction.php

--- PHP/AddNewCatAction.php
+++ PHP/AddNewCatAction.php
@@ -1,6 +1,22 @@
 <?php
 if (isset($_POST['submitCat'])){
-echo "yes";
-}
+   ob_start();
 
+$name=$_POST['NameCat'];
+  
+ require "connect_db.php";
+  
+  //SQL to add a new Man
+  
+  
+  $sql = "INSERT INTO Category (Cat_Name)
+VALUES ('".$name."')";
+
+if ($conn->query($sql) === TRUE) {
+  echo "New record created successfully";
+} else {
+  echo "Error: " . $sql . "<br>" . $conn->error;
+}
+}
+header("Location: ../AddProductAdminPanel.php");
 ?>
