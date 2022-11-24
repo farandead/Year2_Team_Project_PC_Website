fd8ce08b7ce4d23f0e47d6f982c6147f01ad0c84
UshakovKirill <116155318+UshakovKirill@users.noreply.github.com>
2022-11-24 12:46:05 +0000

    Update AddNewProductAction.php

--- PHP/AddNewProductAction.php
+++ PHP/AddNewProductAction.php
@@ -2,7 +2,30 @@
 if (isset($_POST['submitPr'])){
   ob_start();
   
-  echo "Yes";
+  $NamePr=$_POST['NamePr'];
+  $PricePr=$_POST['PricePr'];
+  $DisPr=$_POST['DisPr'];
+  $Cats=$_POST['Cats'];
+  $Mans=$_POST['Mans'];
+  $Predescription=$_POST['Predescription'];
+  $Points=$_POST['Points'];
+  $Description=$_POST['Description'];
+  echo "---------";  
+     echo $NamePr;
+  echo "---------";
+     echo $PricePr;
+  echo "---------";
+     echo $DisPr;
+    echo "---------";
+     echo $Cats;
+    echo "---------";
+     echo $Mans;
+    echo "---------";
+     echo $Predescription;
+    echo "---------";
+     echo $Points;
+    echo "---------";
+     echo $Description;
   
 }
 
