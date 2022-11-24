e8908945d9de0aad8ca7ce6a30a83ff53836052b
UshakovKirill <116155318+UshakovKirill@users.noreply.github.com>
2022-11-24 15:28:11 +0000

    Update upload.php

--- PHP/TEST/upload.php
+++ PHP/TEST/upload.php
@@ -1,5 +1,10 @@
 <?php
 require "../connect_db.php";
+
+$sql= "INSERT INTO ImageTable (Id, Name, Photo) 
+SELECT 1, 'test', BulkColumn 
+FROM Openrowset( Bulk 'Desktop\wall.jpg', Single_Blob) as image";
+
 echo "OK";
 
 ?>
