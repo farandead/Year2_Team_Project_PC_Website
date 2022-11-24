14f9cac9c14fe13d287a32b078f818cfc037a3a8
UshakovKirill <116155318+UshakovKirill@users.noreply.github.com>
2022-11-24 15:29:27 +0000

    Update upload.php

--- PHP/TEST/upload.php
+++ PHP/TEST/upload.php
@@ -5,6 +5,14 @@ $sql= "INSERT INTO ImageTable (Id, Name, Photo)
 SELECT 1, 'test', BulkColumn 
 FROM Openrowset( Bulk 'Desktop\wall.jpg', Single_Blob) as image";
 
+if ($conn->query($sql) === TRUE) {
+  echo "New record created successfully";
+} else {
+  echo "Error: " . $sql . "<br>" . $conn->error;
+}
+
+$conn->close();
+
 echo "OK";
 
 ?>
