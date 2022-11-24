5a3140d322b58fdfed687ef991e0bf50208b42a3
UshakovKirill <116155318+UshakovKirill@users.noreply.github.com>
2022-11-24 19:46:09 +0000

    Update EDITPr.php

--- PHP/EDITPr.php
+++ PHP/EDITPr.php
@@ -3,6 +3,12 @@ require "CheckAdminAccess.php";
 if(isset($_GET['Product'])){
   require "connect_db.php";
   echo "yes";
+  $ID=$_GET['Product'];
+   $sql = "SELECT Product_Price, Product_Discount, Product_Name FROM Product WHERE Product_ID='$ID'";
+  $q=mysqli_query($conn, $sql);
+  $result=mysqli_fetch_array($q);
+  $price=$result['Product_Price'];
+  echo $price;
 }
 
 ?>
