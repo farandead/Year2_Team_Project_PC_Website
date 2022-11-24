5aabdc6875e1298446e0ca1cf3b8b6290de8357b
UshakovKirill <116155318+UshakovKirill@users.noreply.github.com>
2022-11-24 16:07:00 +0000

    Update show.php

--- PHP/TEST/show.php
+++ PHP/TEST/show.php
@@ -1,7 +1,7 @@
 <?php
 require "../connect_db.php";
 
-$sql = "SELECT * FROM TESTUPLOAD WHERE NAME='jk'";
+$sql = "SELECT * FROM TESTUPLOAD WHERE NAME='kj'";
 $sth = $conn->query($sql);
 $result=mysqli_fetch_array($sth);
 echo $result['img'];
