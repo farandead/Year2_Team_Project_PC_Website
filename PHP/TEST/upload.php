06ffb5651e944a876fcf43e5cb87bd7ec84cb9ee
UshakovKirill <116155318+UshakovKirill@users.noreply.github.com>
2022-11-24 15:32:14 +0000

    Update upload.php

--- PHP/TEST/upload.php
+++ PHP/TEST/upload.php
@@ -3,7 +3,7 @@ require "../connect_db.php";
 
 $sql= "INSERT INTO TESTUPLOAD ( NAME, img) 
 SELECT  'test', BulkColumn 
-FROM Openrowset( Bulk 'Desktop\wall.jpg', Single_Blob) as image";
+FROM Openrowset( Bulk 'Desktop\wall.jpg', Single_Blob) as img";
 
 if ($conn->query($sql) === TRUE) {
   echo "New record created successfully";
