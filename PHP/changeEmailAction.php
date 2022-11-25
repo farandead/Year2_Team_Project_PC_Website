755957cfc7551e2592babdd842250b73da018e18
UshakovKirill <116155318+UshakovKirill@users.noreply.github.com>
2022-11-25 21:58:36 +0000

    Update changeEmailAction.php

--- PHP/changeEmailAction.php
+++ PHP/changeEmailAction.php
@@ -4,9 +4,20 @@ require "CheckAdminAccess.php";
 ob_start();
 if(isset($_POST['submitEmail'])){
   echo "Yes:";
-$email=$_GET['EMAIL'];
-$id=$_GET['ID'];
+$email=$_POST['em'];
+$id=base64_decode($_GET['ID']);
   require "connect_db.php";
+  
+  $sql = "UPDATE account SET user_Email='$email' WHERE 	User_ID=$id";
+
+if ($conn->query($sql) === TRUE) {
+  echo "Record updated successfully";
+} else {
+  echo "Error updating record: " . $conn->error;
+}
+  
+   $conn  -> close();
+  
 }
 else{
 header("Location: accounts_base_page.php");
