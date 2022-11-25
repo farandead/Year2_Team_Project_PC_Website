a5a11f256fb74d63c2b0309fdbb6b87c8b92bbd8
UshakovKirill <116155318+UshakovKirill@users.noreply.github.com>
2022-11-25 21:47:28 +0000

    Update changeEmailAction.php

--- PHP/changeEmailAction.php
+++ PHP/changeEmailAction.php
@@ -1,12 +1,12 @@
 <?php
-require "PHP/CheckAdminAccess.php";
+require "CheckAdminAccess.php";
 
 ob_start();
 if(isset($_POST['submitEmail'])){
   echo "Yes:";
 $email=$_GET['EMAIL'];
 $id=$_GET['ID'];
-  
+  require "connect_db.php";
 }
 else{
 header("Location: accounts_base_page.php");
