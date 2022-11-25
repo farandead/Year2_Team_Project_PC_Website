54718ebc70f4ed0aa47577ba1be2d586c0925e87
UshakovKirill <116155318+UshakovKirill@users.noreply.github.com>
2022-11-25 21:14:39 +0000

    Update TableUsersPrinterCollection.php

--- PHP/TableUsersPrinterCollection.php
+++ PHP/TableUsersPrinterCollection.php
@@ -9,7 +9,7 @@ while($row=mysqli_fetch_assoc($query)){
 echo 
                     "<tr>
                       <td>".$row['User_ID']."</td>
-                      <td>".$row['user_Email']." - "."<a href='EditEmailAdminPanel.php?ID".$row['User_ID']."&EMAIL=".$row['user_Email']."'>edit</a>"."</td>
+                      <td>".$row['user_Email']." - "."<a href='EditEmailAdminPanel.php?ID".json_encode($row['User_ID'])."&EMAIL=".json_encode($row['user_Email'])."'>edit</a>"."</td>
                       <td>".$row['User_Phone_Number']."</td>
                       <td>".$row['User_SName']." ".$row['User_FName']."</td>
                   </tr>";
