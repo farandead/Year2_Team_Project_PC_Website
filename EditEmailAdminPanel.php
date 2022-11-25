67d9a09666ef5925337f9273197cff22a524ef9b
UshakovKirill <116155318+UshakovKirill@users.noreply.github.com>
2022-11-25 20:54:42 +0000

    Create EditEmailAdminPanel.php

---
+++ EditEmailAdminPanel.php
@@ -0,0 +1,26 @@
+<?php
+$email=$_GET['EMAIL'];
+
+?>
+<html>
+  <body>
+    <h3>Edit the email<h/3>
+
+    
+ <?php   echo'     <form action ="changeEmail.php?EMAIL='.$email.'" method="post">
+     <h3>'.$email.'</h3>'; ?>
+         
+       <label for="em">Change to:</label><br>
+  <input type="text" id="em" name="em" maxlength= "50" required  ><br><br>
+     
+      
+           
+           
+         
+ 
+     <input type="submit" name="submitEmail" value="Update the email">
+    </form>  
+      
+      
+  </body>
+</html>
