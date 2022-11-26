b47718f9df4ce6032321dbee5b72cf5f43198fab
GIB225 <101982666+GIB225@users.noreply.github.com>
2022-11-26 12:51:55 +0000

    Update logIn_inc.php

--- PHP/logIn_inc.php
+++ PHP/logIn_inc.php
@@ -52,9 +52,9 @@ try {
                         $_SESSION['User_SName'] = $res['User_SName'];
                         $_SESSION['last_login'] = time();
                         session_start();*/
-        $succesM[] = "Success You are connected";
+        //$succesM[] = "Success You are connected";
                         // redirect the user to main page
-                       // header("location: /index.php");
+                       header("location: /index.php");
                      //   exit;
                         //echo "Valid";
                        
