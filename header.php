4e8a39048c184e25f8e507a42aeaa21d562efbd7
farandead <92890148+farandead@users.noreply.github.com>
2022-11-26 20:45:13 +0000

    Add files via upload

--- header.php
+++ header.php
@@ -1,4 +1,4 @@
- <head>
+<head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -41,51 +41,64 @@
         }
     </style>
 
-</head> 
+</head>
 
-<body style="background: none;" class="dark_target"></body>
-   
+<body style="background: none;" class="dark_target">
 
-    <header class="navigationBar" id="navigationLightMode" style="margin-top:-50px" >
-        <ul id="navbar">
+
+    <!-- <header class="navigationBar" id="navigationLightMode" style="margin-top:-50px">
+        <div id="navbar">
             <div id="logoblock">
                 <a href="index.php" class="link">
-                    
-                     <img class="img-logo" src="img/Logo1.png" alt="Logo" > 
-                    <!-- <p id="logoMovie" style="color:white">
+
+                    <img class="img-logo" src="img/Logo1.png" alt="Logo">
+                    <p id="logoMovie" style="color:white">
                         LOGO
 
-                    </p> -->
+                    </p> 
 
                 </a>
 
             </div>
             <div id="linksblock">
-                <li class="navbaritem"><a class="link" href="about_us.php">About Us</a></li>
-                <li class="navbaritem"><a class="link" href="product_page.php?Category=Gaming%20Desktop%20pc">Desktops PCs</a></li>
-                <li class="navbaritem"><a class="link" href="product_page.php?Category=Gaming%20Laptops">Laptops</a></li>
-                <li class="navbaritem"><a class="link" href="contact_us.html">Contact Us</a></li>
-                <li class="navbaritem"><a class="link" href="#">Reviews</a></li>
+                <li class="navbaritem"><a class="link" href="about_us.php">
+                        <p>About Us</p>
+                    </a></li>
+                <li class="navbaritem"><a class="link" href="product_page.php?Category=Gaming%20Desktop%20pc">
+                        <p>Desktops PCs</p>
+                    </a></li>
+                <li class="navbaritem"><a class="link" href="product_page.php?Category=Gaming%20Laptops">
+                        <p>Laptops</p>
+                    </a></li>
+                <li class="navbaritem"><a class="link" href="contact_us.html">
+                        <p>Contact Us</p>
+                    </a></li>
+                <li class="navbaritem"><a class="link" href="#">
+                        <p>Reviews</p>
+                    </a></li>
             </div>
+
 
             <div id="membershipblock">
 
                 <li>
-                <a href="#">
-                    
-                    <div class="icon">
-                        <i class="fa fa-cart-arrow-down" style="font-size: xx-large;color:white"></i>		
-                        		
-                    </div>
-                    
-                    
-                </a></li>
+                    <a href="#">
+
+                        <div class="icon">
+                            <i class="fa fa-cart-arrow-down" style="font-size: xx-large;color:white"></i>
+
+                        </div>
+
+
+                    </a>
+                </li>
                 <br>
                 <br>
 
                 <li>
-                <i id="toggle_button_dark" class="bi bi-moon" onclick="myFunction()"></i> <i id="toggle_button"
-                        style="display: flex;" class="bi bi-brightness-high-fill" onclick="myFunction()"></i></li>
+                    <i id="toggle_button_dark" class="bi bi-moon" onclick="myFunction()"></i> <i id="toggle_button"
+                        style="display: flex;" class="bi bi-brightness-high-fill" onclick="myFunction()"></i>
+                </li>
 
 
                 <br>
@@ -96,9 +109,49 @@
                 <br>
                 <li><a class="sign-in-logo" class="link" href="Sign_up_new.php"> <i class="bi bi-person-fill"
                             style="font-size: xx-large;color:white"></i> </a></li>
-        </ul>
+            </div>
         </div>
 
-    </header>
+    </header> -->
 
+    <div class="navigation-bar"  id="navigationLightMode">
+        <div class="navigation-logo">
 
+            <img class="img-logo" src="img/Logo1.png" alt="Logo">
+
+        </div>
+        <div class="navigation-menu">
+            <ul>
+                <li class="navbaritem"><a class="link" href="product_page.php?Category=Gaming%20Desktop%20pc">
+                        <p>Desktops PCs</p>
+                    </a></li>
+                <li class="navbaritem"><a class="link" href="product_page.php?Category=Gaming%20Laptops">
+                            <p>Laptops</p>
+                </a></li>
+                <li class="navbaritem"><a class="link" href="about_us.php">
+                        <p>About Us</p>
+                    </a></li>
+                <li class="navbaritem"><a class="link" href="contact_us.html">
+                        <p>Contact Us</p>
+                    </a></li>
+                <li class="navbaritem"><a class="link" href="#">
+                        <p>Reviews</p>
+                    </a></li>
+            </ul>
+
+        </div>
+        <div class="navigation-icons">
+
+            <ul>
+                <li>
+                    <i id="toggle_button_dark" class="bi bi-moon" onclick="darkModeProduct()"></i> 
+                    <i id="toggle_button"style="display: flex;color:white;" class="bi bi-brightness-high-fill" onclick="darkModeProduct()"></i></li>
+             
+                <li><a class="sign-in-logo" class="link" href="Sign_up_new.php"> <i class="bi bi-person-fill"
+                            style="color:white"></i> </a></li>
+                <li><i class="fa fa-cart-arrow-down" style="font-size: xx-large;color:white"></i></li>
+
+            </ul>
+        </div>
+    </div>
+</body>
