03005d506d27723d58c2f1d421bdbc71def7a3fc
GIB225 <101982666+GIB225@users.noreply.github.com>
2022-11-27 20:48:46 +0000

    Update user_page.php

--- user_page.php
+++ user_page.php
@@ -1,4 +1,19 @@
-<?php session_start() ?>
+<?php session_start() 
+
+$servername = "localhost";
+$username = 'babimors_astonTeam';
+$password = 'HW#1o.5=#cl+'; 
+
+try {
+    $conn = new PDO("mysql:host=$servername;dbname=babimors_compuk", $username, $password);
+    // set the PDO error mode to exception
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    // echo "Connected successfully"; 
+} catch (PDOException $e) {
+    // echo "Connection failed: " . $e->getMessage();
+}
+
+?>
 <html>
     
     
