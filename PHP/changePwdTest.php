a1df35ee609de40a1452424741e5622e3031afcb
GIB225 <101982666+GIB225@users.noreply.github.com>
2022-11-27 22:53:45 +0000

    Update changePwdTest.php

--- PHP/changePwdTest.php
+++ PHP/changePwdTest.php
@@ -1,4 +1,5 @@
-    <?php
+      
+        <?php
 // Code for change password	
 if(isset($_POST['submit']))
 	{
@@ -36,3 +37,23 @@ $msg="Votre mot de passe à été changer avec succés !";
 
 }
 ?>
+        <div class="col-md-4">
+            <form name="chngpwd" method="post" action="" onSubmit="">
+
+
+ <div class="p-3 py-5">
+                <div class="d-flex justify-content-between align-items-center experience"><span>Changer Mot de Passe</span></div><br>
+                <div class="col-md-12"><label class="labels">Mot de Passe Actuel</label><input type="password" class="form-control" name="password" id="password" required></div> <br>
+                <div class="col-md-12"><label class="labels">Nouveau Mot de Passe</label><input type="password" class="form-control" placeholder="nouveau mot de passe" name="newpassword" id="newpassword" required></div>
+                <div class="col-md-12"><label class="labels">Confirmation Mot de Passe</label><input type="password" class="form-control" placeholder="confirmer mot de passe" name="confirmpassword" id="confirmpassword" required></div>
+            <?php if($error){?><div class="errorWrap"><?php echo htmlentities($error); ?> </div><?php } 
+				else if($msg){?><div class="succWrap"><?php echo htmlentities($msg); ?> </div><?php }?>
+            
+            <div class="mt-5 text-center"><input class="btn   profile-button" type="submit" name="submit" value="Sauvergarder" class="btnSubmit"></div>
+            
+            
+            </div>
+
+</form>
+
+        </div>
