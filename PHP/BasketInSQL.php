1a58974c4b0797298a12b9a61bc58dcb8b84522f
UshakovKirill <116155318+UshakovKirill@users.noreply.github.com>
2022-11-27 10:52:50 +0000

    Update BasketInSQL.php

--- PHP/BasketInSQL.php
+++ PHP/BasketInSQL.php
@@ -7,7 +7,8 @@ $sqlBas = "SELECT * FROM OrderP WHERE User_ID=$id and Order_Status='Basket'";
 if ($resultBas=mysqli_query($conn,$sqlBas)) {
     $rowcount=mysqli_num_rows($resultBas);
     if($rowcount==1){
-    $idOrder=$resultBas['Order_ID'];
+    $RowBas=mysqli_fetch_array($resultBas);
+         $idOrder=$RowBas['Order_ID'];
         $sqlOrder = "SELECT * FROM Linked_Order_and_Products WHERE Order_ID=$idOrder";
         $resultBasPr=mysqli_query($conn,$sqlOrder);
         $rowcount2=mysqli_num_rows($resultBasPr);
