34604e184cf9f9acdac833f825b00c6d0be4d427
GIB225 <101982666+GIB225@users.noreply.github.com>
2022-11-27 16:43:09 +0000

    Update statusTester.php

--- PHP/statusTester.php
+++ PHP/statusTester.php
@@ -5,14 +5,17 @@ echo $_SESSION['User_Status'];
 
 //$_SESSION['User_ID']=29;
 echo $_SESSION['User_ID'];
-if( $_SESSION['login'] = true) {
+
+if($_SESSION['login'] = true) {
   
-                   echo     $_SESSION['UserID'];
-                     echo   $_SESSION['userEmail'] ;
-                      echo  $_SESSION['UserFName'];
-                     echo   $_SESSION['UserSName'] ;
-                     echo   $_SESSION['UserStatus'] ;
-                     echo   $_SESSION['UserPassword'] ;
+  echo $_SESSION['userid'];
+  echo $_SESSION['useremail'] ;
+  echo $_SESSION['userfname'];
+  echo $_SESSION['usersname'] ;
+  echo $_SESSION['userstatus'] ;
+  echo $_SESSION['userpassword'] ;
+ 
+
 }
 
 
