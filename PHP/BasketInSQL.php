f7c44160b193c8e079b7f89882128408f4f341e7
UshakovKirill <116155318+UshakovKirill@users.noreply.github.com>
2022-11-27 10:39:14 +0000

    Update BasketInSQL.php

--- PHP/BasketInSQL.php
+++ PHP/BasketInSQL.php
@@ -1,5 +1,19 @@
 <?php
+$id=29;//$_SESSION['User_ID'];
 $template= "TemplateProductBasket.php";
 $items="";
+
+$sqlBas = "SELECT * FROM OrderP WHERE User_ID=$id and Order_Status='Basket'";
+if ($resultBas=mysqli_query($conn,$sqlBas)) {
+    $rowcount=mysqli_num_rows($resultBas);
+    if($rowcount==1){
+    
+      echo "yes";
+    }else{
+    echo "Something is wrong!";
+    }
+}else{
+echo "Currently empty";
+}
 
 ?>
