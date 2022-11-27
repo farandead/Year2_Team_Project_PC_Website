2ebf2f3a137008e7274c83daca170555a128565c
GIB225 <101982666+GIB225@users.noreply.github.com>
2022-11-27 15:57:35 +0000

    Update statusTester.php

--- PHP/statusTester.php
+++ PHP/statusTester.php
@@ -2,11 +2,12 @@
 session_start();
 //$_SESSION['User_Status']="Admin";
 echo $_SESSION['User_Status'];
+
 //$_SESSION['User_ID']=29;
 echo $_SESSION['User_ID'];
 
 session_start();
-
+echo $_SESSION['User_Status'];
 if (empty($_SESSION['count'])) {
    $_SESSION['count'] = 1;
 } else {
