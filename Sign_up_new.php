97639d7792976f8a208089c7354bcb3da95d3951
GIB225 <101982666+GIB225@users.noreply.github.com>
2022-11-27 19:53:14 +0000

    Update Sign_up_new.php

--- Sign_up_new.php
+++ Sign_up_new.php
@@ -193,6 +193,16 @@ try {
 
 <?php
 
+if($_SESSION['login'] = true) {
+  
+  echo $_SESSION['User_ID'];
+  echo $_SESSION['user_Email'] ;
+  echo $_SESSION['User_FName'];
+  echo $_SESSION['User_SName'] ;
+  echo $_SESSION['User_Status'] ;
+  echo $_SESSION['User_Password'] ;
+                   
+}
 
 
 ?>
