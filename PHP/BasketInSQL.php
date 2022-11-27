c43a2984d4e5ed60e81c89f27b70d34ad0496fef
UshakovKirill <116155318+UshakovKirill@users.noreply.github.com>
2022-11-27 10:48:48 +0000

    Update BasketInSQL.php

--- PHP/BasketInSQL.php
+++ PHP/BasketInSQL.php
@@ -7,7 +7,18 @@ $sqlBas = "SELECT * FROM OrderP WHERE User_ID=$id and Order_Status='Basket'";
 if ($resultBas=mysqli_query($conn,$sqlBas)) {
     $rowcount=mysqli_num_rows($resultBas);
     if($rowcount==1){
-    
+    $idOrder=$resultBas['Order_ID'];
+        $sqlOrder = "SELECT * FROM Linked_Order_and_Products WHERE Order_ID=$idOrder";
+        $resultBasPr=mysqli_query($conn,$sqlOrder);
+        $rowcount2=mysqli_num_rows($resultBasPr);
+        if($rowcount2>0){
+        echo "Good!";
+        }else{
+        echo "Basket is empty";
+        }
+        
+        
+        
       echo "yes";
     }else{
     echo "Something is wrong!";
