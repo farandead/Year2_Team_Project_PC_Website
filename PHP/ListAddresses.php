88716eea7a6afd0ab431084442c5ccd0885c6f3b
UshakovKirill <116155318+UshakovKirill@users.noreply.github.com>
2022-11-28 15:20:59 +0000

    Create ListAddresses.php

---
+++ PHP/ListAddresses.php
@@ -0,0 +1,16 @@
+<?php
+$UserForAddress=$_SESSION['User_ID'];
+$sqlAdr = "SELECT * FROM Delivery_address WHERE User_ID=$UserForAddress";
+$resultAdr=mysqli_query($conn,$sqlAdr);
+while($row=mysqli_fetch_assoc($resultAdr)){
+
+  $addressid=$row['Address_ID'];
+  $address=$row['Country'].$row['City'].$row['Street'].$row['ZIP'];
+  echo ' <option value="".$addressid."">".$address."</option>';
+
+
+}
+
+
+
+?>
