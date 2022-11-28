8bd2987500abc25a4bc61a8784a341ef61bd2f0e
UshakovKirill <116155318+UshakovKirill@users.noreply.github.com>
2022-11-28 15:34:57 +0000

    Update ListAddresses.php

--- PHP/ListAddresses.php
+++ PHP/ListAddresses.php
@@ -3,12 +3,17 @@ $UserForAddress=$_SESSION['User_ID'];
 $sqlAdr = "SELECT * FROM Delivery_address WHERE User_ID=$UserForAddress";
 $resultAdr=mysqli_query($conn,$sqlAdr);
 while($row=mysqli_fetch_assoc($resultAdr)){
-
+  
+  if($row['Address_Status']!="Hidden"){
+$SELECTION=null;
+  if($row['Address_Status']=="Default"){$SELECTION="selected";
+  }
   $addressid=$row['Address_ID'];
   $address=$row['Country'].", ".$row['City'].", ".$row['Street'].", ".$row['ZIP'];
-  echo ' <option value="'.$addressid.'">'.$address.'</option>';
+  
+  echo ' <option value="'.$addressid.'" '.$SELECTION.'>'.$address.'</option>';
 
-
+  }
 }
 
 
