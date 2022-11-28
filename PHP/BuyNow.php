52bc2dc534bf38060e43accd8d17d114b1560a2c
UshakovKirill <116155318+UshakovKirill@users.noreply.github.com>
2022-11-28 14:56:54 +0000

    Update BuyNow.php

--- PHP/BuyNow.php
+++ PHP/BuyNow.php
@@ -22,7 +22,27 @@ if ($resultIDO=mysqli_query($conn,$sqlIDO)) {
       
       
       
-      echo "Yes";
+    $sqlinso = "INSERT INTO Linked_Order_and_Products (Amount_Product, Product_ID, Order_ID)
+VALUES ('1', '$idPr', '$IDorder')";
+
+if ($conn->query($sqlinso) === TRUE) {
+  echo "New record created successfully";
+ 
+ 
+ $sqlUPORDER = "UPDATE OrderP SET Order_Status='InProcess' WHERE 	Order_ID=$IDorder";
+
+if ($conn->query($sqlUPORDER) === TRUE) {
+  echo "Record updated successfully";
+} else {
+  echo "Error updating record: " . $conn->error;
+}
+ 
+ 
+ 
+ 
+} else {
+  echo "Error: " . $sqlinso . "<br>" . $conn->error;
+}
       
       
       
