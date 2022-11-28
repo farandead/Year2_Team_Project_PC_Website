a13888fc7ea6abeb4d1390c3cb28f5bc6521ae0b
UshakovKirill <116155318+UshakovKirill@users.noreply.github.com>
2022-11-28 12:10:37 +0000

    Update updateOrderSt.php

--- PHP/updateOrderSt.php
+++ PHP/updateOrderSt.php
@@ -1,4 +1,12 @@
 <?php
-echo $_GET['OI'];
-
+ob_start();
+if(isset($_POST['updateStO'])){
+$OrderID= base64_decode($_GET['OI']);
+$Status=$_POsT['Statuses'];
+  echo $OrderID;
+  echo $Status;
+  
+ require "connect_db.php";
+  
+}
 ?>
