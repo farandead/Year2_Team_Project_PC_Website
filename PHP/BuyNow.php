991b899ee9c0d3a49b191d45d3bcf4645cca49e0
UshakovKirill <116155318+UshakovKirill@users.noreply.github.com>
2022-11-28 14:45:23 +0000

    Update BuyNow.php

--- PHP/BuyNow.php
+++ PHP/BuyNow.php
@@ -1,7 +1,42 @@
 <?php
 if(isset($_POST['buy'])){
+ob_start();
+ session_start();
+  require "connect_db.php";
+$idPr= $_POST['id'];
+$Price= $_POST['price'];
+$User=$SESSION['User_ID'];
+  
+  $sql = "INSERT INTO OrderP (Order_Status, Total_Price, User_ID)
+VALUES ('BuyNow', '$Price', '$User')";
 
-  echo $_POST['id']."<br>";echo $_POST['price'];
+if ($conn->query($sql) === TRUE) {
+  echo "New record created successfully";
+  
+$sqlIDO = "SELECT Order_ID FROM OrderP WHERE User_ID=$User AND Order_Status= 'BuyNow'";
+if ($resultIDO=mysqli_query($conn,$sqlIDO)) {
+    $rowcount=mysqli_num_rows($resultIDO);
+    if($rowcount=1){
+    $IDorder=mysqli_fetch_assoc($resultIDO);
+        
+      
+      
+      
+      echo "Yes";
+      
+      
+      
+    }
+    else{echo "Failed";}
+}else{echo "Failed";}
+  
+  
+  
+} else {
+  echo "Error: " . $sql . "<br>" . $conn->error;
+}
+
+  
 echo "yes";
 }else{echo "no";}
 
