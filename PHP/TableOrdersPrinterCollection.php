6d77dc56f7f29a3251bbff6126d691e0e2e653bc
UshakovKirill <116155318+UshakovKirill@users.noreply.github.com>
2022-11-28 10:07:20 +0000

    Create TableOrdersPrinterCollection.php

---
+++ PHP/TableOrdersPrinterCollection.php
@@ -0,0 +1,24 @@
+<?php
+
+require "connect_db.php";
+
+$templateRow="";
+$sql="SELECT * FROM OrderP WHERE NOT Order_Status ='Basket'";
+  $query=mysqli_query($conn, $sql);
+while($row=mysqli_fetch_assoc($query)){
+echo "
+
+                <tr>
+                  <td>".$row['Order_ID']."</td>
+                  <td>Items</td>
+                  <td>".$row['User_ID']."</td>
+                  <td>Status</td>
+                  <td>Total ".$row['Total_Price']."</td>
+              </tr>";
+  
+
+}
+
+$conn  -> close();
+
+?>
