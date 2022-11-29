7438b96171a8cfeadde850a6964b1e9841836fc5
UshakovKirill <116155318+UshakovKirill@users.noreply.github.com>
2022-11-29 11:52:02 +0000

    Update ShoppingCart.php

--- ShoppingCart.php
+++ ShoppingCart.php
@@ -47,7 +47,7 @@ session_start();
         <div class="">
              <form action ="PHP/CheckOutBasket.php" method="post">
         <?php
-                 if(isset($_SESSION['User_Status'])){echo "yes";}else{echo "no";}
+                 if(isset($_SESSION['User_Status'])){echo '<input type="submit" name="submitCat" value="Check out!">';}else{echo "no";}
                  
                  
    include "PHP/BasketCollectionPrinter.php";
