008b0d6b2c4a78eeee8d3b9739be077704680950
UshakovKirill <116155318+UshakovKirill@users.noreply.github.com>
2022-11-29 10:16:29 +0000

    Update TableAdminsPrinterCollection.php

--- PHP/TableAdminsPrinterCollection.php
+++ PHP/TableAdminsPrinterCollection.php
@@ -15,9 +15,9 @@ while($row=mysqli_fetch_assoc($query)){
                 <td>'.$row['User_Phone_Number'].'</td>
                 <td>'.$row['User_SName'].' '.$row['User_FName'].'</td>
                 <td><div class="btn-group 1" role="group" aria-label="Basic checkbox toggle button group">
-                    <a href="PHP/acceptornotadmin.php?a=1&?who='.$UserID.'"><input type="button" class="btn-check 1" id="btncheck1 1" autocomplete="off">
+                    <a href="PHP/acceptornotadmin.php?a=1&who='.$UserID.'"><input type="button" class="btn-check 1" id="btncheck1 1" autocomplete="off">
                     <label class="btn btn-outline-dark" for="btncheck1 1">Admin</label></a>
-                   <a href="PHP/acceptornotadmin.php?a=0&?who='.$UserID.'">
+                   <a href="PHP/acceptornotadmin.php?a=0&who='.$UserID.'">
                     <input type="button" class="btn-check 1" id="btncheck2 1" autocomplete="off">
                     <label class="btn btn-outline-dark" for="btncheck2 1">Non-Admin</label></a>
                   </div></td>
