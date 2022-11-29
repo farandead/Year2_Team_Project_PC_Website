662eddb5488e36e27f675a359b053f68474cabe6
UshakovKirill <116155318+UshakovKirill@users.noreply.github.com>
2022-11-29 09:41:51 +0000

    Create TableAdminsPrinterCollection.php

---
+++ PHP/TableAdminsPrinterCollection.php
@@ -0,0 +1,27 @@
+<?php
+$templateRow="";
+$sql="SELECT * FROM account WHERE  User_Status ='RegAdmin'";
+  $query=mysqli_query($conn, $sql);
+
+while($row=mysqli_fetch_assoc($query)){
+  $UserID=base64_encode($row['Order_ID']);
+  
+  echo 
+    '<tr>
+                <td>ID#</td>
+                <td>Email@gmail.com</td>
+                <td>071234567893</td>
+                <td>Name</td>
+                <td><div class="btn-group 1" role="group" aria-label="Basic checkbox toggle button group">
+                    <input type="checkbox" class="btn-check 1" id="btncheck1 1" autocomplete="off">
+                    <label class="btn btn-outline-dark" for="btncheck1 1">Admin</label>
+                  
+                    <input type="checkbox" class="btn-check 1" id="btncheck2 1" autocomplete="off">
+                    <label class="btn btn-outline-dark" for="btncheck2 1">Non-Admin</label>
+                  </div></td>
+            </tr>';
+  
+  
+}
+
+?>
