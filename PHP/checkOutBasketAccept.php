44a102d06f70735483faa391738f8e15a0d1a1f4
UshakovKirill <116155318+UshakovKirill@users.noreply.github.com>
2022-11-29 16:24:19 +0000

    Update checkOutBasketAccept.php

--- PHP/checkOutBasketAccept.php
+++ PHP/checkOutBasketAccept.php
@@ -2,23 +2,7 @@
 ob_start();
 if(isset($_POST['buyB'])){
 echo "1";
-}elseif(isset($_POST['Cancel'])){echo "2";
-                                 
- $idor=$_POST['id'];          
-
-
-  $sqlBuyNow = "UPDATE OrderP SET 	Order_Status='InProcess' WHERE Order_ID=$idor";
-
-if ($conn->query($sqlBuyNow) === TRUE) {
-  echo "Record updated successfully";
-} else {
-  echo "Error updating record: " . $conn->error;
-}
-  
-          
-    header("Location: ../ShoppingCart.php");                             
-                                 
-                                 
+                  
 }else{
 header("Location: ../index.php");
 }
