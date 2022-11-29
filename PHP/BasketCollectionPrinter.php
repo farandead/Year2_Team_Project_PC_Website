ffb1bbf846c7c3d1776359782a3552460912a2fe
UshakovKirill <116155318+UshakovKirill@users.noreply.github.com>
2022-11-29 16:53:09 +0000

    Update BasketCollectionPrinter.php

--- PHP/BasketCollectionPrinter.php
+++ PHP/BasketCollectionPrinter.php
@@ -4,6 +4,7 @@ if(isset($_SESSION['User_Status'])){
   require "connect_db.php";
   require "BasketInSQL.php";
   require "Printer.php";
+  if($rowcount==1){echo '<input type="submit" name="Update" value="Update the basket">';echo '<input type="submit" name="CheckOut" value="Check out!">';}
   
 }else{
 if(isset($_COOKIE["BasketCompUK"])){
