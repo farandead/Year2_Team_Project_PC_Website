600fc1938428e999fa1c1c0d11fc20d9da4e3f62
UshakovKirill <116155318+UshakovKirill@users.noreply.github.com>
2022-11-29 12:27:17 +0000

    Update CheckOutBasket.php

--- PHP/CheckOutBasket.php
+++ PHP/CheckOutBasket.php
@@ -1,7 +1,25 @@
 <?php
 if(isset($_POST['Update'])){
 echo "Update";
-  print_r($_POST);
+  require "connect_db.php";
+  
+  $sql="SELECT * FROM Linked_Order_and_Products WHERE  Order_ID ='$idor'";
+  $query=mysqli_query($conn, $sql);
+
+while($row=mysqli_fetch_assoc($query)){
+$PRid=$row['Product_ID'];
+$amount=$_POST['$PRid'];
+$sqlUp = "UPDATE Linked_Order_and_Products SET 	Amount_Product='$amount' WHERE Order_ID ='$idor' AND Product_ID='$PRid'";
+
+if ($conn->query($sqlUp) === TRUE) {
+  echo "Record updated successfully";
+} else {
+  echo "Error updating record: " . $conn->error;
+}
+
+
+}
+  
 }
 if(isset($_POST['CheckOut'])){
 echo "CheckOut";
