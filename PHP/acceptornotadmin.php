086cda6486b0a4e995558f4b40c8f3f8924d393c
UshakovKirill <116155318+UshakovKirill@users.noreply.github.com>
2022-11-29 10:27:35 +0000

    Update acceptornotadmin.php

--- PHP/acceptornotadmin.php
+++ PHP/acceptornotadmin.php
@@ -1,8 +1,27 @@
 <?php
 require "CheckAdminAccess.php";
 if(isset($_GET['a']) and isset($_GET['who'])){
-echo "yes";
+  ob_start();
+require "connect_db.php";
+ $userID=base64_decode($_GET['who']);
+ if($_GET['a']==1){$status="Admin";
+ }else{$status="Customer";
+ }
+  
+  
+  $sql = "UPDATE account SET User_Status='$status' WHERE User_ID=$userID";
+
+if ($conn->query($sql) === TRUE) {
+  echo "Record updated successfully";
+} else {
+  echo "Error updating record: " . $conn->error;
 }
 
+$conn->close();
+  
+  
+  
+}
+header("Location: ../index.php");
 
 ?>
