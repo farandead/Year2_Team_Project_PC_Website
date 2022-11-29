4f9886f4b5e761f76b9f06e52475a3c94ce9504c
UshakovKirill <116155318+UshakovKirill@users.noreply.github.com>
2022-11-29 16:41:39 +0000

    Update checkOutBasketAccept.php

--- PHP/checkOutBasketAccept.php
+++ PHP/checkOutBasketAccept.php
@@ -1,8 +1,32 @@
 <?php
-ob_start();
-if(isset($_POST['buyB'])){
-echo "1";
-                  
+ob_start();session_start();
+if(isset($_POST['buyB']) and isset($SESSION['login'])){
+$idor=$_POST['id'];
+  $idad=$_POST['addresses'];
+  
+  require "connect_db.php";
+      //link address and order
+$sql = "INSERT INTO Linked_Address_and_Order (Order_ID, Address_ID)
+VALUES ('$idor', '$idad')";
+
+if ($conn->query($sql) === TRUE) {
+  echo "New record created successfully";
+} else {
+  echo "Error: " . $sql . "<br>" . $conn->error;
+}  
+  
+  
+     //update status to inptocess
+  $sql = "UPDATE OrderP SET Order_Status	='InProcess' WHERE Order_ID=$idor";
+
+if ($conn->query($sql) === TRUE) {
+  echo "Record updated successfully";
+} else {
+  echo "Error updating record: " . $conn->error;
+}
+  
+  //go to profile page!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!1
+  echo "done";
 }else{
 header("Location: ../index.php");
 }
