7e7d70b12d4186a7d045e4b2da72ac0969f200ba
UshakovKirill <116155318+UshakovKirill@users.noreply.github.com>
2022-11-29 15:03:18 +0000

    Update printProductsCheckOut.php

--- PHP/printProductsCheckOut.php
+++ PHP/printProductsCheckOut.php
@@ -2,11 +2,15 @@
 $sqlPrL="SELECT * FROM Linked_Order_and_Products WHERE  Order_ID ='$orderID'";
   $queryPrL=mysqli_query($conn, $sqlPrL);
 while($row=mysqli_fetch_assoc($queryPrL)){
+  
+  $price=round($row['Product_Price']-$row['Product_Price']*$row['Product_Discount']/100,2);
+  $priceForAmount=$price*$row['Amount_Product'];
+  
   $prodid=$row['Product_ID'];
 $sqlPr="SELECT * FROM Linked_Order_and_Products WHERE  Product_ID ='$prodid'";
 $queryPr=mysqli_query($conn, $sqlPr);
   $productinfo=mysqli_fetch_assoc($queryPr);
-  echo $productinfo['Product_ID'];
+  echo $productinfo['Product_Name']."(x".$row['Amount_Product'].")    =£". $priceForAmount;
   echo "<br>";
 
 }
