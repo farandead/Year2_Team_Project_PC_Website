ee63c745015ba29b3fd1102ed781f076839aa597
GIB225 <101982666+GIB225@users.noreply.github.com>
2022-11-30 10:50:49 +0000

    Update userprofile.php

--- userprofile.php
+++ userprofile.php
@@ -6,6 +6,7 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js">
     <link rel="stylesheet" href="css/index.css">
     <link rel="stylesheet" href="css/header.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.3.0/font/bootstrap-icons.css" />
 </head>
 
 <?php 
