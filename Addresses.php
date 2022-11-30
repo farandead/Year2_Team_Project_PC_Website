76ebee1632d9ff1526db52b3cf4b7407b1a4f07b
UshakovKirill <116155318+UshakovKirill@users.noreply.github.com>
2022-11-30 14:05:41 +0000

    Create Addresses.php

---
+++ Addresses.php
@@ -0,0 +1,21 @@
+html>
+<body>
+
+
+<h1>Addresses panel</h1>
+
+<form action="/action_page.php">
+  <p>Please select the address:</p>
+  <input type="radio" id="1" name="address" value="1">
+  <label for="html">address</label><br>
+  <input type="radio" id="2" name="address" value="2">
+  <label for="css">address</label><br>
+  <input type="radio" id="3" name="address" value="3">
+  <label for="javascript">address</label>
+
+<br>
+<input type="submit" value="Remove">
+<input type="submit" value="Make deafault">
+</form>
+</body>
+</html>
