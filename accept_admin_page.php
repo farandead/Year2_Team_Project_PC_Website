0ed9c656fda14b2b91e5017722c75e28ca7b725b
Shoka-San <aaronj.willis17@gmail.com>
2022-11-30 10:41:43 +0000

    .

--- accept_admin_page.php
+++ accept_admin_page.php
@@ -4,92 +4,101 @@ require "PHP/CheckAdminAccess.php";
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Movie -Your One Stop Source of Unlimited Movies</title>
-    
-    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-<script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
-    
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Staatliches&display=swap" rel="stylesheet">
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Russo+One&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="css/mainpage.css">
-    <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-    <link rel="stylesheet" href="https://unpkg.com/flickity@2/dist/flickity.min.css">
-    <script src="https://unpkg.com/flickity@2/dist/flickity.pkgd.min.js"></script>
-    
-   
-    
-    <link href="https://fonts.googleapis.com/css?family=Open+Sans:300i,400" rel="stylesheet">
-    <link rel="stylesheet" href="css/slidshow.css">
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Staatliches&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.3.0/font/bootstrap-icons.css" />
-    <script src="js/toggle_switch.js"></script>
-    
-    
-   
-   
-
-</head>
-<div class="navigation-bar"  id="navigationLightMode">
-        <div class="navigation-logo">
-
-         <a  style="text-decoration: none;" href="index.php"><img class="img-logo" src="img/Logo1.png" alt="Logo"></a>
-
-        </div>
-        <div class="navigation-menu">
-            <ul>
-                <li class="navbaritem"><a class="link" href="product_page.php?Category=Gaming%20Desktop%20pc">
-                        <p style="color:white">Desktops PCs</p>
-                    </a></li>
-                <li class="navbaritem"><a class="link" href="product_page.php?Category=Gaming%20Laptops">
-                            <p style="color:white">Laptops</p>
-                </a></li>
-                <li class="navbaritem"><a class="link" href="about_us.php">
-                        <p style="color:white">About Us</p>
-                    </a></li>
-                <li class="navbaritem"><a class="link" href="contact_us.php">
-                        <p style="color:white">Contact Us</p>
-                    </a></li>
-                <li class="navbaritem"><a class="link" href="#">
-                        <p style="color:white">Reviews</p>
-                    </a></li>
-            </ul>
-
-        </div>
-        <div class="navigation-icons">
-
-            <ul>
-                    <li><i id="toggle_button_dark" class="bi bi-moon" onclick="darkModeProduct()"></i></li> 
-                    <li><i id="toggle_button"style="display: flex;color:white;" class="bi bi-brightness-high-fill" onclick="darkModeProduct()"></i></li>
-                    <li><a href="ShoppingCart.php"><i class="fa fa-cart-arrow-down" style="font-size: xx-large;color:white"></i></a></li>
-                    
-<?php if (isset($_SESSION['login'])){ ?>
-                    
-                    <li><a title="Profile page" href="userprofile.php" class="link sign-in-logo dropdown-toggle" type="link" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><i class="bi bi-person-fill" style="color:white"> </i></a></li>
-                    <li><a title="Log out" class="dropdown-item" href="PHP/signOut.inc.php"><i class="fa fa-power-off" aria-hidden="true"></i></a></li>
-   <?php }else { ?>
-                    <li><a class="sign-in-logolink" href="Sign_up_new.php"><i class="bi bi-person-fill" style="color:white"></i></a></li>
-     <?php } ?>
-
-
-            </ul>
-                
-               
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>COMPUK</title>
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Staatliches&display=swap" rel="stylesheet">
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Russo+One&display=swap" rel="stylesheet">
+        <link rel="stylesheet" href="css/mainpage.css">
+        <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+        <link rel="stylesheet" href="https://unpkg.com/flickity@2/dist/flickity.min.css">
+        <script src="https://unpkg.com/flickity@2/dist/flickity.pkgd.min.js"></script>
+        <link href="https://fonts.googleapis.com/css?family=Open+Sans:300i,400" rel="stylesheet">
+        <link rel="stylesheet" href="css/admin.css">
+        <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.2.0/css/bootstrap.min.css">
+        <link rel="stylesheet" href="https://cdn.datatables.net/1.13.1/css/dataTables.bootstrap5.min.css">
         
+    
+    
+        <link rel="stylesheet" href="css/slidshow.css">
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Staatliches&display=swap" rel="stylesheet">
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.3.0/font/bootstrap-icons.css" />
+        <script src="js/toggle_switch.js"></script>
+        <script>
+    
+        </script>
+        <style>
+            body {
+                margin-top: 30px;
+                padding: 0%;
+                font-family: "Arial", Helvetica, sans-serif;
+            }
+    
+            i {
+                font-size: 30px;
+                cursor: pointer;
+                position: relative;
+                ;
+            }
+        </style>
+    
+    <!-- font-family: 'Staatliches', cursive; -->
+    <!-- font-family: 'Russo One', sans-serif; -->
+    
+    <body style="background-image: none;">
+    
+        <header class="container-fluid navigationBar" id="navigationLightMode" style="margin-top: -50px;">
+            <div class="row">
+            <ul id="navbar">
+                <div id="logoblock">
+                    <a href="mainpage.html" class="link">
+                        <p id="logoMovie" style="color:white">
+                            LOGO
+    
+                        </p>
+                    </a>
+    
+                </div>
                 
-        </div>
+                <div id="linksblock">
+                    <li class="navbaritem"><a class="link" href="filmPage.html">DESKTOP PCs</a></li>
+                    <li class="navbaritem"><a class="link" href="#">LAPTOPS</a></li>
+                    <li class="navbaritem"><a class="link" href="#">CONTACT US</a></li>
+                    <li class="navbaritem"><a class="link" href="#">REVIEWS</a></li>
+                </div>
+    
+                <div id="membershipblock">
+    
+    
+    
+                    <li><i id="toggle_button_dark" class="bi bi-moon" onclick="myFunction()"></i> <i id="toggle_button"
+                            style="display: flex;" class="bi bi-brightness-high-fill" onclick="myFunction()"></i></li>
+    
+    
+                    <br>
+                    <br>
+                    <li><a class="searchLogo" class="link" href=""> <i class="fa fa-search"
+                                style="font-size: xx-large;color:white"></i> </a></li>
+                    <br>
+                    <br>
+                    <li><a class="sign-in-logo" class="link" href="Sign_up.html"> <i class="bi bi-person-fill"
+                                style="font-size: xx-large;color:white"></i> </a></li>
+                       
+                  
+         
                 
-    </div>
+                </div>
+            </div>
+            </ul>
+            </ul>
             
             <!--Admin Panel-->
         <div class="container-fluid">
@@ -133,7 +142,7 @@ require "PHP/CheckAdminAccess.php";
 
     <div class="container1 mt-5 d-flex justify-content-center align-items-center mt-5 py-5">
       <div class="row"></div>
-      <h1>Adnin authorisation</h1>
+      <h1>Admin Authorisation</h1>
     </div>
   </div>
 
