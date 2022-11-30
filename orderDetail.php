7adf60100e741e55662ce2b99aed6971dac5aa92
GIB225 <101982666+GIB225@users.noreply.github.com>
2022-11-30 22:43:09 +0000

    Update orderDetail.php

--- orderDetail.php
+++ orderDetail.php
@@ -29,9 +29,9 @@ session_start();
     <?php require 'header.php'; ?>
 
 
-<div class="shopping-box">
+<!-- <div class="shopping-box">
     <div class="shopping_con">
-        <div class="shopping_title">PRODUCT LIST</div> <?php if(!(isset($_SESSION['login']))){echo '<a href= "Sign_up_new.php"> Sign in to check out.</a>';} ?>
+        <div class="shopping_title">ORDER DETAIL</div> 
         <div class="">
              <form action ="PHP/CheckOutBasket.php" method="post">
         <?php
@@ -47,7 +47,51 @@ session_start();
                  if(isset($TOTAL)){echo "Total: £ ". $TOTAL;}}
     ?>
             
-         </form>
+         </form> -->
+    
+    <?php
+//$lines= file("Products/PreDescription/".$row['Product_ID'].".txt");
+$price=round($row['Product_Price']-$row['Product_Price']*$row['Product_Discount']/100,2);
+echo "<div class=\"devices-sub-container-product\">
+                        <div class=\"productdetails dark_target\">
+                            <div class=\"product-details-title\">".$row['Product_Name']."</div>
+                            <div class=\"product-details-img\"><img src=\"../Products/Images/".$row['Product_ID'].".jpg\"  width=\"150 px\" height=\"150px\" alt=\"No image\">
+                            </div>
+                            <div class=\"product-details-description dark_target\">
+                                <p>Price: <span>".$price."£</span> ";
+                                if($row['Product_Discount']!=0){
+                               echo ",discount is ".$row['Product_Discount']."% (<s>".$row['Product_Price']."£</s>)";
+                                }
+                                echo "
+                                </p>
+                                <ul class=\"description-ulisted\">";
+                                
+//foreach($lines as $line){   
+//echo $ArrayCollectionBasketDic[$row['Product_ID']];
+                                   echo " <li class=\"description-ulisted-item\"> <h3>Quantity:</h3><h4><input type=\"number\" value=\"".$ArrayCollectionBasketDic[$row['Product_ID']]."\" id=\"quantity\" name=\"".$row['Product_ID']."\" min=\"1\" max=\"5\"></h4>"."</li>";
+//}
+if(isset($_SESSION['User_Status'])){$DeleteLink="PHP/RemoveBasketIn.php?ITR=";
+}else{$DeleteLink="PHP/RemoveBasketOut.php?ITR=";
+}
+// <button> does not work due to the form!
+                               echo "     
+                                </ul>
+                            </div>
+                            <div class=\"product-details-buy-buttons\">
+                               <a href='Product_Description.php?Product=".$row['Product_ID']."'> LOOK UP THIS ITEM ></a>
+                            </div>
+  <div class=\"product-details-buy-buttons\">
+                               <a href='".$DeleteLink.$row['Product_ID']."'> REMOVE THIS ITEM ></a>
+                            </div>";
+
+echo "
+      
+                        </div>
+                    </div>";
+
+
+
+?>
             
             
             
