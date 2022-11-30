db30a15b3b7179002c3a10f519b5020433206576
GIB225 <101982666+GIB225@users.noreply.github.com>
2022-11-30 22:58:17 +0000

    Update orderDetail.php

--- orderDetail.php
+++ orderDetail.php
@@ -1,6 +1,17 @@
 <?php 
 session_start();
- echo $_GET['orderId']    ;  
+ $servername = "localhost";
+$username = 'babimors_astonTeam';
+$password = 'HW#1o.5=#cl+'; 
+
+try {
+    $conn = new PDO("mysql:host=$servername;dbname=babimors_compuk", $username, $password);
+    // set the PDO error mode to exception
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    // echo "Connected successfully"; 
+} catch (PDOException $e) {
+    // echo "Connection failed: " . $e->getMessage();
+}
 ?>
 <!DOCTYPE html>
 <html lang="en">
