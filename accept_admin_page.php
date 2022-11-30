f2cf2e6ab70aa5dadb1a8c8cac4ee5469ba9130b
Shoka-San <aaronj.willis17@gmail.com>
2022-11-30 12:22:21 +0000

    .

--- accept_admin_page.php
+++ accept_admin_page.php
@@ -7,7 +7,7 @@ require "PHP/CheckAdminAccess.php";
 
 <head>
 <?php include 'header.php'; ?>
-<head>
+
         <meta charset="UTF-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
