bf88fca24caaac1d8f46a4f0289aab97e0078bc7
GIB225 <101982666+GIB225@users.noreply.github.com>
2022-11-30 21:21:57 +0000

    Update userprofile.php

--- userprofile.php
+++ userprofile.php
@@ -118,7 +118,7 @@ try {
                     <?php
                     
                     $test = "38";
-                $sql = "SELECT * FROM OrderP WHERE User_ID=? WHERE Order_Status != 'Basket' ";
+                $sql = "SELECT * FROM OrderP WHERE User_ID=? AND Order_Status != 'Basket' ";
                 $result_order = $conn->prepare($sql);
                 $result_order->execute(array($test));  /* $_SESSION['User_ID']*/
                 $count_order = $result_order->rowCount();
