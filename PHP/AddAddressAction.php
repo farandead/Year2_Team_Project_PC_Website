599131cc6c1ea03696cc5a9649e4d2b42c0f8b14
UshakovKirill <116155318+UshakovKirill@users.noreply.github.com>
2022-11-30 15:11:58 +0000

    Update AddAddressAction.php

--- PHP/AddAddressAction.php
+++ PHP/AddAddressAction.php
@@ -1,9 +1,26 @@
 <?php
 if(isset($_POST['AddAddress'])){
-echo "yes";
+ session_start();
+ ob_start();
+  
+  $userID=$_SESSION['User_ID'];
+  $ZIP=$_POST['ZIP'];
+  $City=$_POST['City'];
+  $Country=$_POST['Country'];
+  $Street=$_POST['Street'];
+    
+/*  $sql = "INSERT INTO Delivery_address(firstname, lastname, email)
+VALUES ('John', 'Doe', 'john@example.com')";
+
+if ($conn->query($sql) === TRUE) {
+  echo "New record created successfully";
+} else {
+  echo "Error: " . $sql . "<br>" . $conn->error;
+}*/
+
 
 }
-
+//header("Location:../userprofile.php");
 
 
 ?>
