6e7bde3ed120298f1200603e2c68b0882eca7882
GIB225 <101982666+GIB225@users.noreply.github.com>
2022-11-30 01:24:06 +0000

    Create signOut.inc.php

---
+++ PHP/signOut.inc.php
@@ -0,0 +1,15 @@
+<?php
+session_start();
+require_once'/PHP/connect_db.php';
+
+ 
+  if(isset($_SESSION['login'])){
+
+             session_unset();
+             session_destroy();
+             header("location: /index.php");
+              die;
+      
+ }else{  
+     header("location: /signUp_inc.php");
+ }
