e693faefbc8841760df2727c61c453212f484e8a
Shoka-San <aaronj.willis17@gmail.com>
2022-11-30 11:23:49 +0000

    .

--- orders_management.php
+++ orders_management.php
@@ -1,41 +1,23 @@
 <?php require "PHP/CheckAdminAccess.php"; ?>
 <!DOCTYPE html>
 <html lang="en">
+<link rel="stylesheet" href="css/header.css">
+
+<head>
+<?php include 'header.php'; ?>
 <head>
         <meta charset="UTF-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>COMPUK</title>
-        <link rel="preconnect" href="https://fonts.googleapis.com">
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-        <link href="https://fonts.googleapis.com/css2?family=Staatliches&display=swap" rel="stylesheet">
-        <link rel="preconnect" href="https://fonts.googleapis.com">
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-        <link href="https://fonts.googleapis.com/css2?family=Russo+One&display=swap" rel="stylesheet">
-        <link rel="stylesheet" href="css/mainpage.css">
-        <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-        <link rel="stylesheet" href="https://unpkg.com/flickity@2/dist/flickity.min.css">
-        <script src="https://unpkg.com/flickity@2/dist/flickity.pkgd.min.js"></script>
-        <link href="https://fonts.googleapis.com/css?family=Open+Sans:300i,400" rel="stylesheet">
         <link rel="stylesheet" href="css/admin.css">
         <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.2.0/css/bootstrap.min.css">
         <link rel="stylesheet" href="https://cdn.datatables.net/1.13.1/css/dataTables.bootstrap5.min.css">
         
     
-    
-        <link rel="stylesheet" href="css/slidshow.css">
-        <link rel="preconnect" href="https://fonts.googleapis.com">
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-        <link href="https://fonts.googleapis.com/css2?family=Staatliches&display=swap" rel="stylesheet">
-        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.3.0/font/bootstrap-icons.css" />
-        <script src="js/toggle_switch.js"></script>
-        <script>
-    
         </script>
         <style>
             body {
-                margin-top: 30px;
+                margin-top:;
                 padding: 0%;
                 font-family: "Arial", Helvetica, sans-serif;
             }
@@ -47,59 +29,10 @@
                 ;
             }
         </style>
-    
-    <!-- font-family: 'Staatliches', cursive; -->
-    <!-- font-family: 'Russo One', sans-serif; -->
-    
-    <body style="background-image: none;">
-    
-        <header class="container-fluid navigationBar" id="navigationLightMode" style="margin-top: -50px;">
-            <div class="row">
-            <ul id="navbar">
-                <div id="logoblock">
-                    <a href="mainpage.html" class="link">
-                        <p id="logoMovie" style="color:white">
-                            LOGO
-    
-                        </p>
-                    </a>
-    
-                </div>
-                
-                <div id="linksblock">
-                    <li class="navbaritem"><a class="link" href="filmPage.html">DESKTOP PCs</a></li>
-                    <li class="navbaritem"><a class="link" href="#">LAPTOPS</a></li>
-                    <li class="navbaritem"><a class="link" href="#">CONTACT US</a></li>
-                    <li class="navbaritem"><a class="link" href="#">REVIEWS</a></li>
-                </div>
-    
-                <div id="membershipblock">
-    
-    
-    
-                    <li><i id="toggle_button_dark" class="bi bi-moon" onclick="myFunction()"></i> <i id="toggle_button"
-                            style="display: flex;" class="bi bi-brightness-high-fill" onclick="myFunction()"></i></li>
-    
-    
-                    <br>
-                    <br>
-                    <li><a class="searchLogo" class="link" href=""> <i class="fa fa-search"
-                                style="font-size: xx-large;color:white"></i> </a></li>
-                    <br>
-                    <br>
-                    <li><a class="sign-in-logo" class="link" href="Sign_up.html"> <i class="bi bi-person-fill"
-                                style="font-size: xx-large;color:white"></i> </a></li>
-                       
-                  
-         
-                
-                </div>
-            </div>
-            </ul>
-            </ul>
-            
+</head>
+<header>
             <!--Admin Panel-->
-            <div class="container-fluid">
+        <div class="container-fluid">
             <div class="row">
     <nav class="navbar navbar-expand-lg bg-dark navbar-dark px-4" style="margin-top: -15px;">
         <a href="#" class="navbar-brand">Admin</a>
@@ -130,18 +63,12 @@
 </div>
 </header>
 
-
-<!--Orders Management-->
+        </body>
+<!--Accept Admin Table-->
 <section class ="table">
     <div class="container1 d-flex justify-content-center align-items-center py-2">
       <div class="row"></div>
-      <h1>'</h1>
-    </div>
-  </div>
-
-    <div class="container1 mt-5 d-flex justify-content-center align-items-center mt-5 py-5">
-      <div class="row"></div>
-      <h1>Orders Management Table</h1>
+      <h1>Orders management</h1>
     </div>
   </div>
 
