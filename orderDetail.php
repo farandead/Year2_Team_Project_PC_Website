4a9bdacdde974e04f9e634e6cfec7a16c76a07fd
GIB225 <101982666+GIB225@users.noreply.github.com>
2022-12-01 01:48:29 +0000

    Update orderDetail.php

--- orderDetail.php
+++ orderDetail.php
@@ -103,7 +103,6 @@ echo "<div class=\"devices-sub-container-product\">
                                 
 //foreach($lines as $line){   
 //echo $ArrayCollectionBasketDic[$row['Product_ID']];
-                                   echo " <li class=\"description-ulisted-item\"> <h3>Quantity:</h3><h4><input type=\"number\" value=\"".$ArrayCollectionBasketDic[$row['Product_ID']]."\" id=\"quantity\" name=\"".$row['Product_ID']."\" min=\"1\" max=\"5\"></h4>"."</li>";
 //}
 if(isset($_SESSION['User_Status'])){$DeleteLink="PHP/RemoveBasketIn.php?ITR=";
 }else{$DeleteLink="PHP/RemoveBasketOut.php?ITR=";
