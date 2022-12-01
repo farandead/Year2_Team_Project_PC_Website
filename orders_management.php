cc6611040b763b65e73f526b5802b697d7aadd88
Shoka-San <aaronj.willis17@gmail.com>
2022-12-01 17:55:38 +0000

    /

--- orders_management.php
+++ orders_management.php
@@ -4,6 +4,8 @@
 <link rel="stylesheet" href="css/header.css">
 
 <head>
+<?php include 'header.php'; ?>
+
         <meta charset="UTF-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
