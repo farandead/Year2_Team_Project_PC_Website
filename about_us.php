2d2c1ae3fa38a452b7590600eb34ae99d4e30572
farandead <92890148+farandead@users.noreply.github.com>
2022-12-01 18:42:21 +0000

    Update about_us.php

--- about_us.php
+++ about_us.php
@@ -30,18 +30,7 @@
 
     </script>
     <style>
-        body {
-            margin: 0%;
-            padding: 0%;
-            font-family: "Arial", Helvetica, sans-serif;
-        }
-
-        i {
-            font-size: 30px;
-            cursor: pointer;
-            position: relative;
-            ;
-        }
+  
     </style>
 
 </head>
