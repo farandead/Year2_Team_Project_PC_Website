1f3613bc7578c25432128aad0449fda1d78dd10c
farandead <92890148+farandead@users.noreply.github.com>
2022-12-01 18:07:40 +0000

    Update orders_management.php

--- orders_management.php
+++ orders_management.php
@@ -24,21 +24,7 @@
 <script defer src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
         
     
-        </script>
-        <style>
-            body {
-                margin-top:;
-                padding: 0%;
-                font-family: "Arial", Helvetica, sans-serif;
-            }
-    
-            i {
-                font-size: 30px;
-                cursor: pointer;
-                position: relative;
-                ;
-            }
-        </style>
+ 
 </head>
 <header>
             <!--Admin Panel-->
