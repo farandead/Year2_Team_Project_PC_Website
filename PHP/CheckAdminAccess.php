8da0f4c954f017aaad716d5cdcdad74b74bd0ad8
UshakovKirill <116155318+UshakovKirill@users.noreply.github.com>
2022-12-01 16:05:05 +0000

    Update CheckAdminAccess.php

--- PHP/CheckAdminAccess.php
+++ PHP/CheckAdminAccess.php
@@ -1,10 +1,14 @@
 <?php
 
 session_start();
+require "connect_db.php";
+$idu=$_SESSION['User_ID'];
+$sqlAdminS="SELECT User_Status FROM account WHERE User_ID=$idu";
+$resultF = $conn->query($sqlAdminS);
+$INFO = $resultF->fetch_assoc()
 
-
-if(isset($_SESSION['User_Status'])){
-$status=$_SESSION['User_Status'];}else{$status=null;}
+if(isset($INFO['User_Status'])){
+$status=$INFO['User_Status'];}else{$status=null;}
 
 
 if(!($status=="Admin")){
