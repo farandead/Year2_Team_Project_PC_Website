a46f05fe316e5fca376fc0f8c57c8a4742872859
farandead <92890148+farandead@users.noreply.github.com>
2022-12-01 17:54:06 +0000

    Update accept_admin_page.php

--- accept_admin_page.php
+++ accept_admin_page.php
@@ -102,6 +102,14 @@ require "PHP/CheckAdminAccess.php";
       <script src="https://cdn.datatables.net/1.13.1/js/jquery.dataTables.min.js"></script>
       <script src="https://cdn.datatables.net/1.13.1/js/dataTables.bootstrap5.min.js"></script>
       <script src="./js/admin_data_table.js"></script>
+<script src="//code.jquery.com/jquery-3.5.1.js"></script>
+<script src="https://cdn.datatables.net/1.13.1/js/jquery.dataTables.min.js"></script>
+<script src="https://cdn.datatables.net/1.13.1/js/dataTables.bootstrap5.min.js"></script>
+<script src="./js/admin_data_table.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js"></script> 
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script> 
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/js/bootstrap.min.js"></script>
+
 
       
 </body>
