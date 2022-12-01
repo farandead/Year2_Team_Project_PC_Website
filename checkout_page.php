1f577d31470306ab93a6b74cd8fce16c5057974d
GIB225 <101982666+GIB225@users.noreply.github.com>
2022-12-01 05:47:10 +0000

    Update checkout_page.php

--- checkout_page.php
+++ checkout_page.php
@@ -47,6 +47,8 @@ header("Location: index.php");
 <link rel="stylesheet" href="css/header.css">
     <script src="js/toggle_switch.js"></script>
     <link rel="stylesheet" href="css/checkout_page.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.3.0/font/bootstrap-icons.css" />
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 
 
     <style>
