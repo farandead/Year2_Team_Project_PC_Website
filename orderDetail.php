8228c79f37bfe28811172a97e4439de19cce9303
GIB225 <101982666+GIB225@users.noreply.github.com>
2022-12-01 02:20:43 +0000

    Update orderDetail.php

--- orderDetail.php
+++ orderDetail.php
@@ -1,17 +1,6 @@
 <?php 
 session_start();
- $servername = "localhost";
-$username = 'babimors_astonTeam';
-$password = 'HW#1o.5=#cl+'; 
-
-try {
-    $conn = new PDO("mysql:host=$servername;dbname=babimors_compuk", $username, $password);
-    // set the PDO error mode to exception
-    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    // echo "Connected successfully"; 
-} catch (PDOException $e) {
-    // echo "Connection failed: " . $e->getMessage();
-}
+require "PHP/connect_db.php";
 ?>
 <!DOCTYPE html>
 <html lang="en">
