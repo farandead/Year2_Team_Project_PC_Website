841a12e7bc1d9bf7a951ec5a9dd932f6ac6781a1
GIB225 <101982666+GIB225@users.noreply.github.com>
2022-12-01 05:36:17 +0000

    Update AddProductAdminPanel.php

--- AddProductAdminPanel.php
+++ AddProductAdminPanel.php
@@ -31,6 +31,7 @@ $queryCats=mysqli_query($conn,"SELECT * FROM Category");
     <link href="https://fonts.googleapis.com/css2?family=Staatliches&display=swap" rel="stylesheet">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.3.0/font/bootstrap-icons.css"/>
+     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 
     <link rel="stylesheet" href="css/form.css">
     <link rel="stylesheet" href="css/addPagemainpage.css">
