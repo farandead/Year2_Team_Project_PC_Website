781840aa6f8ae70f2b05b5a740965a2cacf1263d
UshakovKirill <116155318+UshakovKirill@users.noreply.github.com>
2022-12-01 16:51:34 +0000

    Update about_us.php

--- about_us.php
+++ about_us.php
@@ -93,9 +93,9 @@
             </section>
             <section class="cards-container">
                 <div class="card card--current" id="1">
-                    <h1>A. Lorem ipsum dolor sit amet</h1>
+                    <h1>A. test Lorem ipsum dolor sit amet</h1>
                     <p>
-                        Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                        trst Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
                         tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
                         quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
                         consequat.
