a4780c8676be75202cae7e7690e352515013dec9
GIB225 <101982666+GIB225@users.noreply.github.com>
2022-12-01 05:01:03 +0000

    Update checkout_page_basket.php

--- checkout_page_basket.php
+++ checkout_page_basket.php
@@ -31,7 +31,7 @@ require "PHP/connect_db.php";
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Staatliches&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.3.0/font/bootstrap-icons.css" />
-
+<link rel="stylesheet" href="css/header.css">
     <script src="js/toggle_switch.js"></script>
     <link rel="stylesheet" href="css/checkout_page.css">
 
