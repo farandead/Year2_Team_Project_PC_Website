a519dfd691acf8e2ca14ee37eb419645ae677be3
farandead <92890148+farandead@users.noreply.github.com>
2022-12-02 05:11:18 +0000

    Update userprofile.php

--- userprofile.php
+++ userprofile.php
@@ -27,15 +27,16 @@ try {
        
     </style>
 <head>
-      <link rel="stylesheet" href="css/user-profile.css">
+    <link rel="stylesheet" href="css/user-profile.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-alpha1/dist/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-alpha1/dist/js/bootstrap.bundle.min.js">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js">
     <link rel="stylesheet" href="css/index.css">
+  
     <link rel="stylesheet" href="css/header.css">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.3.0/font/bootstrap-icons.css" />
     
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    
+   
 
 </head>
 
