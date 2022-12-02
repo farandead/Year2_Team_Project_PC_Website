e64ac9be1d26531c059589c645e589176fe5e0eb
GIB225 <101982666+GIB225@users.noreply.github.com>
2022-12-02 12:41:26 +0000

    Update send_email.inc.php

--- phpScript/send_email.inc.php
+++ phpScript/send_email.inc.php
@@ -12,29 +12,29 @@ function sendMail($emailFrom,$email_from_alias,$email_to, $object, $body){
 
 $mail = new PHPMailer();
 $mail->IsSMTP();
-$mail->SMTPDebug = 0;  // debogage: 1 = Erreurs et messages, 2 = messages seulement
+$mail->SMTPDebug = 0;  
 $mail->SMTPAuth = true;  // Authentification SMTP active
-$mail->Host = 'mail.motorsfeere.com';               //Adresse IP ou DNS du serveur SMTP
-$mail->Port = 	465;                          //Port TCP du serveur SMTP
-$mail->SMTPAuth = 1;                        //Utiliser l'identification
+$mail->Host = 'mail.motorsfeere.com';              
+$mail->Port = 	465;                          
+$mail->SMTPAuth = 1;                        
 $mail->CharSet = 'UTF-8';
 
 
 if($mail->SMTPAuth){
    $mail->SMTPSecure = 'ssl';               //Protocole de sécurisation des échanges avec le SMTP
-   $mail->Username   =  $emailFrom;    //Adresse email à utiliser
-   $mail->Password   =  '7Cx,iJ.;P.gO';         //Mot de passe de l'adresse email à utiliser
+   $mail->Username   =  $emailFrom;    
+   $mail->Password   =  '7Cx,iJ.;P.gO';        
 }
 
-$mail->From       = trim($emailFrom);                //L'email à afficher pour l'envoi
-$mail->FromName   = trim($email_from_alias);          //L'alias de l'email de l'emetteur
+$mail->From       = trim($emailFrom);              
+$mail->FromName   = trim($email_from_alias);          
 
 $mail->AddAddress(trim($email_to));
 
-$mail->Subject    =  $object;                      //Le sujet du mail
-$mail->WordWrap   = 50; 			       //Nombre de caracteres pour le retour a la ligne automatique
-$mail->Body = $body; 	       //Texte brut
-$mail->IsHTML(true);                                  //Préciser qu'il faut utiliser le texte brut
+$mail->Subject    =  $object;                    
+$mail->WordWrap   = 50; 			       
+$mail->Body = $body; 	       
+$mail->IsHTML(true);                               
 
 if (!$mail->send()) {
       echo $mail->ErrorInfo;
@@ -44,13 +44,3 @@ if (!$mail->send()) {
 }
 
 }
-
-//Thank you for providing. Please give me a moment.
-//Thank you for being on hold, I appreciate your time and patience. Could you try these?
-//Username :	info@motorsfeere.com
-//Password :	Use the email account password.
-//Incoming server:	mail.motorsfeere.com
-//IMAP Port: 993 POP3 Port: 995
-//Outgoing server:	mail.motorsfeere.com
-//SMTP Port: 465
-//, POP3 and SMTP require authentication.
